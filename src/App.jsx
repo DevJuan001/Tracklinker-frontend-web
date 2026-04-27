993714591a8218fa63cbdd03421fb8365f14573d
DevJuan001 <juanesyt7@gmail.com>
2026-04-27 15:41:54 -0500

    feat: implement main application structure with routing and theme support

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./router/AppRouter";
+import { useTheme } from "./globals/hooks/useTheme";
+
+function App() {
+  useTheme();
+  return (
+    <div className="w-screen h-screen dark:bg-[#0a0a0a]">
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+      <div id="modal-root"></div>
+    </div>
+  );
+}
+
+export default App;
