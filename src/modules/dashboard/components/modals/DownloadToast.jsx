cdb4831956e18c7a367e4d09fa270a89550aa59c
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 18:49:41 -0500

    feat(DownloadToast): update pointer-events style for improved interaction

--- src/modules/dashboard/components/modals/DownloadToast.jsx
+++ src/modules/dashboard/components/modals/DownloadToast.jsx
@@ -8,7 +8,7 @@ export default function DownloadToast({ showDownloadToast, onClose }) {
   return (
     <section
       className="fixed inset-0 z-10 flex items-start justify-center gap-5 pr-5 pb-5 pt-5 animate-toastIn
-    dark:text-white pointer-events-auto"
+    dark:text-white pointer-events-none"
     >
       <div
         className="relative border bg-green-200 rounded-full shadow-md shadow-green-300 py-1.5 px-3 animate-toastIn
