6ba1b7963f33c3e53018cb494bbdda9f67330402
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 18:49:50 -0500

    feat(TopChartsCard): update hover scale effect for improved interaction

--- src/modules/dashboard/components/ui/TopChartsCard.jsx
+++ src/modules/dashboard/components/ui/TopChartsCard.jsx
@@ -4,7 +4,7 @@ export default function TopChartsCard({ title, children, background }) {
   return (
     <section
       className={`row-span-1 col-span-3 flex flex-col p-4 shadow-xl border border-gray-200 rounded-xl transition duration-500 
-        hover:scale-105
+        hover:scale-[1.03]
         dark:bg-[#0f0f11] dark:border-transparent dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:text-white ${background}`}
     >
       <section>
