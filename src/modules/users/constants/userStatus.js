01dd628f8b164464b309c68ab50736d0f2fefa95
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 17:15:34 -0500

    feat(userStatus): update icon references and styles for consistency

--- src/modules/users/constants/userStatus.js
+++ src/modules/users/constants/userStatus.js
@@ -1,18 +1,21 @@
-import { usersIcons } from "../../../assets/icons/usersIcons";
+import Icon from "../../../globals/components/ui/Icon";
 
 export const userStatus = {
   1: {
     text: "Deshabilitado",
     modalType: "enable",
+    fill: false,
     visibilityIcon: false,
-    icon: usersIcons.inactiveCircle,
-    styles: "w-32 bg-gray-50 text-gray-400 border-gray-300 dark:border-gray-700 dark:bg-[#75777e80]",
+    icon: "block",
+    styles:
+      "w-32 bg-gray-50 text-gray-400 border-gray-300 dark:border-gray-700 dark:bg-[#75777e80]",
   },
   2: {
     text: "Activo",
     modalType: "disable",
+    fill: true,
     visibilityIcon: true,
-    icon: usersIcons.activeCircle,
+    icon: "circle",
     styles:
       "w-20 bg-green-200 text-green-600 border-green-300 dark:border-green-900 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
   },
