00a86ca254a412f13f0254600ecd7fd5fcf27c4e
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 18:49:46 -0500

    feat(ChartCard): adjust hover scale effect for improved interaction

--- src/modules/dashboard/components/ui/ChartCard.jsx
+++ src/modules/dashboard/components/ui/ChartCard.jsx
@@ -17,7 +17,7 @@ export default function ChartCard({
     <section
       className={`row-span-${rowSpan} col-span-${colSpan} bg-${bgColor} text-${textColor} dark:text-white
         flex flex-col p-3 shadow-xl border border-gray-200 rounded-xl transition duration-500
-        hover:bg-gray-200 hover:scale-105
+        hover:bg-gray-200 hover:scale-[1.02]
         dark:bg-[#0f0f11] dark:border-transparent dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:hover:bg-[#2c2c2e]
         `}
     >
