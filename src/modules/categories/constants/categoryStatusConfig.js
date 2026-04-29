a98f0fddb698a1454f10440f803b30e2791afc83
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 21:50:46 -0500

    feat(categoryStatusConfig): update icon references and improve style consistency

--- src/modules/categories/constants/categoryStatusConfig.js
+++ src/modules/categories/constants/categoryStatusConfig.js
@@ -1,20 +1,20 @@
-import { usersIcons } from "../../../assets/icons/usersIcons";
-
 export const categoryStatusConfig = {
   1: {
     text: "Deshabilitada",
     modalType: "enable",
     visibilityIcon: false,
-    icon: usersIcons.inactiveCircle,
+    icon: "block",
+    fill: false,
     styles:
-      "w-32 bg-gray-50 text-gray-400 border-gray-300 dark:border-gray-700 dark:bg-[#75777e80]",
+      "w-32 bg-gray-50 text-gray-400 border-gray-200 dark:border-gray-800 dark:bg-[#75777e80]",
   },
   2: {
     text: "Activa",
     modalType: "disable",
     visibilityIcon: true,
-    icon: usersIcons.activeCircle,
+    fill: true,
+    icon: "circle",
     styles:
-      "w-20 bg-green-200 text-green-600 border-green-300 dark:border-green-900 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
+      "w-20 bg-green-200 text-green-600 border-green-200 dark:border-green-950 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
   },
 };
