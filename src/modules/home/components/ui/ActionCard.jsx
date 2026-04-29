cbe9922f074076efa1e60112351d9e0c335817ab
DevJuan001 <juanesyt7@gmail.com>
2026-04-29 12:44:56 -0500

    feat(ActionCard): refactor component structure and improve icon rendering

--- src/modules/home/components/ui/ActionCard.jsx
+++ src/modules/home/components/ui/ActionCard.jsx
@@ -1,11 +1,7 @@
 import { NavLink } from "react-router-dom";
+import Icon from "../../../../globals/components/ui/Icon";
 
-export default function ActionCard({
-  itemName,
-  itemPath,
-  itemIcon: Icon,
-  itemAlt,
-}) {
+export default function ActionCard({ itemName, itemPath, itemIcon }) {
   return (
     <section>
       {/* Card de cada modulo */}
@@ -19,17 +15,19 @@ export default function ActionCard({
         sm:h-36 sm:w-32"
       >
         <section
-          className="h-20 w-20 rounded-xl bg-[#eae8eb] content-center justify-items-center
+          className="h-20 w-20 rounded-xl bg-[#eae8eb] flex items-center justify-center
           dark:bg-[#101012]
           xl:h-40 xl:w-32
           md:h-40 md:w-32
           sm:h-40 sm:w-32"
         >
           <Icon
-            aria-label={itemAlt}
-            className="w-8 h-10 fill-[#75777E] 
-            md:w-10 md:h-10
-            xl:w-10 xl:h-10"
+            name={itemIcon}
+            size={40}
+            fill
+            color={"#75777E"}
+            className="w-8 h-10
+            md:w-11 md:h-11"
           />
         </section>
         <p
