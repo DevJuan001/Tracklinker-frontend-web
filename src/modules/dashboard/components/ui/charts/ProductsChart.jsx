dabea4a7e023a993fe4ef0530b968464b53d2b16
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 21:32:55 -0500

    feat(ProductsChart): simplify component by removing loading/error states and enhancing rendering structure

--- src/modules/dashboard/components/ui/charts/ProductsChart.jsx
+++ src/modules/dashboard/components/ui/charts/ProductsChart.jsx
@@ -2,28 +2,18 @@ import TopChartsCard from "../TopChartsCard";
 import { useProductChart } from "../../../hooks/useProductChart";
 
 export default function ProductsChart() {
-  const { productChartInfo, loading, error } = useProductChart();
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
+  const { productChartInfo } = useProductChart();
 
   return (
-    <TopChartsCard background={"products-background"} title={"Productos"}>
+    <>
       {productChartInfo.map((item) => (
-        <div className="flex items-center gap-1" key={item.products}>
-          {/* Productos Totales */}
-          <span className="font-semibold text-2xl">{item.products}</span>
-          {/* Productos que entraron en el mes actual */}
-          <span className="font-medium text-base text-[#00a86b]">
-            +{item.new_products}
-          </span>
-        </div>
+        <TopChartsCard
+          background={"products-background"}
+          title={"Productos"}
+          metricValue={item.products}
+          growth={item.new_products}
+        />
       ))}
-    </TopChartsCard>
+    </>
   );
 }
