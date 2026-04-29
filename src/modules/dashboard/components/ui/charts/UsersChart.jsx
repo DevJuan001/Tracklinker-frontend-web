c60395e62ca8b81c3bc38d7a7f280184722c30a3
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 21:32:04 -0500

    feat(UsersChart): simplify loading and error handling, enhance chart rendering

--- src/modules/dashboard/components/ui/charts/UsersChart.jsx
+++ src/modules/dashboard/components/ui/charts/UsersChart.jsx
@@ -2,28 +2,20 @@ import TopChartsCard from "../TopChartsCard";
 import { useUsersChart } from "../../../hooks/useUsersChart";
 
 export default function UsersChart() {
-  const { usersChartInfo, loading, error } = useUsersChart();
-
-  if (loading) {
-    return <div>Cargando...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const { usersChartInfo } = useUsersChart();
 
   return (
-    <TopChartsCard background={"users-background"} title={"Usuarios"}>
+    <>
       {usersChartInfo.map((item) => (
-        <div className="flex items-center gap-1" key={item.users}>
-          {/* Usuarios existentes */}
-          <span className="font-semibold text-2xl">{item.users}</span>
-          {/* Usuarios creados en el mes actual */}
-          <span className="font-medium text-base text-[#00a86b]">
-            +{item.new_users}
-          </span>
-        </div>
+        <TopChartsCard
+          background={
+            item.new_users > 0 ? "growth-background" : "users-background"
+          }
+          title={"Usuarios"}
+          metricValue={item.users}
+          growth={item.new_users}
+        />
       ))}
-    </TopChartsCard>
+    </>
   );
 }
