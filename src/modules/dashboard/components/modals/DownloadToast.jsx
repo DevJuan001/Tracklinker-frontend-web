63fcb0bc015802b936a2db0b5d6a763f58568959
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 21:34:43 -0500

    feat(DownloadToast): enhance shadow effects for improved visual feedback

--- src/modules/dashboard/components/modals/DownloadToast.jsx
+++ src/modules/dashboard/components/modals/DownloadToast.jsx
@@ -11,8 +11,8 @@ export default function DownloadToast({ showDownloadToast, onClose }) {
     dark:text-white pointer-events-none"
     >
       <div
-        className="relative border bg-green-200 rounded-full shadow-md shadow-green-300 py-3 px-4 animate-toastIn
-      dark:bg-green-700 dark:border-[#ffffff26] dark:shadow-[0px_0px_20px_-1px_#16a34a]"
+        className="relative border bg-green-200 rounded-full shadow-[0px_0px_20px_-5px_#86efac] py-3 px-4 animate-toastIn
+      dark:bg-green-800 dark:border-[#ffffff26] dark:shadow-[0px_0px_20px_-1px_#16a34a]"
       >
         <span className="font-medium text-center text-green-600 dark:text-white">
           Descarga exitosa
