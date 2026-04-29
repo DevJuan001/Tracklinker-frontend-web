35db714a09af6f4d0a5440275a9e791ae4792f95
DevJuan001 <juanesyt7@gmail.com>
2026-04-28 21:32:50 -0500

    feat(OutputOrdersChart): refactor component to simplify rendering and enhance structure

--- src/modules/dashboard/components/ui/charts/OutputOrdersChart.jsx
+++ src/modules/dashboard/components/ui/charts/OutputOrdersChart.jsx
@@ -2,22 +2,18 @@ import TopChartsCard from "../TopChartsCard";
 import { useOutputOrdersChart } from "../../../hooks/useOutputOrdersChart";
 
 export default function OutputOrdersChart() {
-  const { orders, loading, error } = useOutputOrdersChart();
+  const { orders } = useOutputOrdersChart();
+
   return (
-    <TopChartsCard
-      background={"output-orders-background"}
-      title={"Ordenes de salida"}
-    >
+    <>
       {orders.map((item) => (
-        <div className="flex items-center gap-1" key={item.orders}>
-          {/* Ordenes existentes */}
-          <span className="font-semibold text-2xl">{item.orders}</span>
-          {/* Ordenes creadas en el mes actual */}
-          <span className="font-medium text-base text-[#00a86b]">
-            +{item.new_orders}
-          </span>
-        </div>
+        <TopChartsCard
+          background={"output-orders-background"}
+          title={"Ordenes de salida"}
+          metricValue={item.orders}
+          growth={item.new_orders}
+        />
       ))}
-    </TopChartsCard>
+    </>
   );
 }
