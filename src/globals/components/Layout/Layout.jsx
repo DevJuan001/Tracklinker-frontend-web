fe50fbf7d15c9586e726509f009639009b28d89a
DevJuan001 <juanesyt7@gmail.com>
2026-04-30 13:22:34 -0500

    feat(Layout): update layout structure to use flexbox for improved responsiveness

--- src/globals/components/Layout/Layout.jsx
+++ src/globals/components/Layout/Layout.jsx
@@ -4,15 +4,15 @@ export default function Layout({ avatarOnClick, helpOnClick, children }) {
   return (
     // Container
     <div
-      className="min-h-screen min-w-screen max-w-screen max-h-screen grid grid-cols-1 grid-rows-[1fr_80px]
-        md:grid-cols-[110px_1fr] md:grid-rows-1
+      className="min-h-screen min-w-screen max-w-screen max-h-screen flex flex-col
+        md:grid md:grid-cols-[110px_1fr] md:grid-rows-1
         xl:grid-cols-[260px_1fr] xl:grid-rows-1
         "
     >
       <Aside avatarOnClick={avatarOnClick} helpOnClick={helpOnClick} />
       <main
-        className="min-w-full h-full pl-0.5 pr-3 py-2 dark:bg-black overflow-hidden order-1
-        md:order-2 md:py-4 md:max-h-full
+        className="min-w-full h-full pl-0.5 pr-3 py-2 dark:bg-black overflow-y-auto order-1
+        md:order-2 md:py-4 md:max-h-full md:overflow-hidden
         xl:order-2 xl:pb-3 xl:max-h-full"
       >
         {children}
