cfc751600570f9ed3881ebada313403a1175d7fb
DevJuan001 <juanesyt7@gmail.com>
2026-05-01 15:19:37 -0500

    feat(Aside): change class from absolute to sticky for improved layout behavior

--- src/globals/components/Layout/Aside.jsx
+++ src/globals/components/Layout/Aside.jsx
@@ -21,7 +21,7 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
 
   return (
     <aside
-      className="absolute bottom-0 flex order-2 h-[80px] py-1 z-10
+      className="sticky bottom-0 flex order-2 h-[80px] py-1 z-10
       md:static md:h-full md:flex-col md:order-1 md:px-5 md:pt-5
       xl:h-full xl:flex-col xl:row-span-2 xl:px-3 xl:pt-5 xl:order-1
       dark:bg-black"
