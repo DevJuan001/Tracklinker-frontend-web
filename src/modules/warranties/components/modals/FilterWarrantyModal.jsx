978f3c5d8ca85f2a000d66006493263463d1d3cc
DevJuan001 <juanesyt7@gmail.com>
2026-05-01 19:23:47 -0500

    feat(FilterWarrantyModal): refactor to use setFilters and integrate city selection

--- src/modules/warranties/components/modals/FilterWarrantyModal.jsx
+++ src/modules/warranties/components/modals/FilterWarrantyModal.jsx
@@ -1,16 +1,11 @@
+import { useCities } from "../../../../globals/hooks/useCities";
 import { useFilterWarranties } from "../../hooks/useFilterWarranties";
 import SelectMenu from "../../../../globals/components/modals/SelectMenu";
 import FilterModal from "../../../../globals/components/modals/FilterModal";
 
-export default function FilterWarrantyModal({ refetch, onClose }) {
-  const { form, handleChange, handleApply } = useFilterWarranties(
-    {
-      start_date: "",
-      end_date: "",
-      status: "",
-    },
-    refetch,
-  );
+export default function FilterWarrantyModal({ setFilters, onClose }) {
+  const { form, handleChange } = useFilterWarranties();
+  const { cities } = useCities();
 
   return (
     <FilterModal
@@ -20,20 +15,33 @@ export default function FilterWarrantyModal({ refetch, onClose }) {
       orderByFinishDateOnChange={handleChange}
       onClose={onClose}
       applyButtonOnClick={() => {
+        setFilters({ ...form });
         onClose();
-        handleApply();
       }}
     >
       <div className="flex flex-col gap-2">
+        <SelectMenu
+          searchable
+          name={"city"}
+          spanText={"Ciudad"}
+          value={form.city}
+          onChange={handleChange}
+          options={cities.map((city) => ({
+            value: city.id,
+            label: city.name,
+          }))}
+        />
+
         <SelectMenu
           name={"status"}
           value={form.status}
           onChange={handleChange}
           spanText={"Estado"}
           options={[
-            { value: 1, label: "Pendiente" },
-            { value: 2, label: "En Proceso" },
-            { value: 3, label: "Completada" },
+            { value: 1, label: "Deshabilitada" },
+            { value: 2, label: "Pendiente" },
+            { value: 3, label: "En Proceso" },
+            { value: 4, label: "Completada" },
           ]}
         />
       </div>
