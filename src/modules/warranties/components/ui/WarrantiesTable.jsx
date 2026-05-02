41c4c39bbc8f15eaf44a2c6b695c3398441acc36
DevJuan001 <juanesyt7@gmail.com>
2026-05-01 19:23:30 -0500

    feat(WarrantiesTable): enhance no results display and update warranty data mapping

--- src/modules/warranties/components/ui/WarrantiesTable.jsx
+++ src/modules/warranties/components/ui/WarrantiesTable.jsx
@@ -1,11 +1,26 @@
-import { actionsIcons } from "../../../../assets/icons/actionsIcons";
 import { warrantyStatusConfig } from "../../constants/warrantyStatus";
 import ActionButtons from "../../../../globals/components/ui/ActionButtons";
+import Icon from "../../../../globals/components/ui/Icon";
 
-export default function WarrantiesTable({ warranties, openModal, refetch }) {
-  return (
-    <section className="max-h-[95%] max-w-full border border-gray-200 rounded-xl shadow-md overflow-y-auto overflow-x-auto overflow-hidden dark:border-[#303033]">
-      <table className="min-w-full min-h-full border-collapse dark:text-white">
+export default function WarrantiesTable({ warranties, openModal }) {
+  const noWarranties = !Array.isArray(warranties) || warranties.length === 0;
+
+  return noWarranties ? (
+    <div className="flex flex-col items-center justify-center gap-4 text-[#7E7775]">
+      <Icon name={"search_off"} size={75} />
+      <span className="text-xl font-medium">
+        No se han encontrado resultados
+      </span>
+    </div>
+  ) : (
+    <section
+      className="max-h-[95%] max-w-full border border-gray-200 rounded-3xl overflow-y-auto overflow-x-auto overflow-hidden
+    dark:border-[#303033]"
+    >
+      <table
+        className="min-w-full min-h-full border-collapse
+      dark:text-white"
+      >
         <thead className="sticky top-0 z-1">
           <tr className="h-[40px] border-b border-gray-200 dark:border-[#303033] text-sm">
             <th className="font-medium text-start pl-4">Estado</th>
@@ -16,7 +31,6 @@ export default function WarrantiesTable({ warranties, openModal, refetch }) {
             <th className="font-medium text-start pl-4">Teléfono</th>
             <th className="font-medium text-start pl-4">Dirección</th>
             <th className="font-medium text-start pl-4">Ciudad</th>
-            <th className="font-medium text-start pl-4">N° de caso</th>
             <th className="font-medium text-center pl-4">Acción</th>
           </tr>
         </thead>
@@ -25,72 +39,69 @@ export default function WarrantiesTable({ warranties, openModal, refetch }) {
         <tbody className="font-normal dark:text-white">
           {warranties.map((warranty) => (
             <tr
-              onClick={(e) => {
-                e.stopPropagation();
-                openModal(warranty, "info", refetch);
-              }}
-              key={warranty.warranty_incidents_id}
-              className="h-12 text-base overflow-x-auto overflow-y-auto transition duration-500 text-[#45474d] cursor-pointer
-                          hover:bg-[#e3e2e4] hover:shadow-md 
-                          dark:hover:bg-[#2d2d30] dark:text-white"
+              key={warranty.id}
+              className="relative h-12 text-base overflow-x-auto overflow-y-auto transition duration-75 text-[#45474d]
+              hover:bg-[#F5F3F6]
+              dark:hover:bg-[#2d2d30] dark:text-white"
             >
               <th className="font-normal text-start pl-3 text-sm">
                 <div
                   className={`w-fit flex items-center pl-1.5 pr-3 py-0.5 gap-1.5 rounded-full border dark:border-transparent
-                    ${warrantyStatusConfig[warranty.warranty_status]?.styles}
+                    ${warrantyStatusConfig[warranty.status]?.styles}
                     `}
                 >
-                  <img
-                    src={warrantyStatusConfig[warranty.warranty_status]?.icon}
-                    className="w-4 h-4 dark:brightness-[0.5]"
+                  <Icon
+                    name={warrantyStatusConfig[warranty.status]?.icon}
+                    size={16}
+                    fill={warrantyStatusConfig[warranty.status]?.fill}
                   />
                   <span
-                    className={` ${warrantyStatusConfig[warranty.warranty_status]?.textColor}`}
+                    className={`text-nowrap ${warrantyStatusConfig[warranty.status]?.textColor}`}
                   >
-                    {warrantyStatusConfig[warranty.warranty_status]?.text}
+                    {warrantyStatusConfig[warranty.status]?.text}
                   </span>
                 </div>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_date}</p>
+                <p>{warranty.date}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_customer}</p>
+                <p>{warranty.customer}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_description}</p>
+                <p>{warranty.description}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
                 <p>{warranty.product_serial}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_phone}</p>
+                <p>{warranty.phone}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_address}</p>
+                <p>{warranty.address}</p>
               </th>
               <th className="font-normal text-start pl-4 text-sm">
-                <p>{warranty.warranty_city}</p>
+                <p>{warranty.city_name}</p>
               </th>
-              <th className="font-normal text-start pl-6 text-sm">
-                <p>{warranty.warranty_incidents_id}</p>
-              </th>
-              <th className="font-normal text-end text-sm">
+              <th className="relative text-end text-sm">
                 <ActionButtons
                   editButtonOnClick={(e) => {
                     e.stopPropagation();
-                    openModal(warranty, "edit", refetch);
+                    openModal(warranty, "edit", null, e.currentTarget);
                   }}
                   deleteButtonOnClick={(e) => {
                     e.stopPropagation();
-                    openModal(warranty, "delete", refetch);
+                    openModal(warranty, "delete", null, e.currentTarget);
                   }}
                 >
                   <button
-                    className="dark:brightness-200 hover:scale-125 transition-all duration-500"
-                    onClick={() => openModal(warranty, "info", refetch)}
+                    onClick={(e) =>
+                      openModal(warranty, "info", null, e.currentTarget)
+                    }
+                    className="flex items-center transition-colors duration-300 rounded-xl p-1.5
+                    hover:bg-[#969292a8]"
                   >
-                    <img src={actionsIcons.moreInfoIcon} alt="Más Info" />
+                    <Icon name={"arrow_outward"} />
                   </button>
                 </ActionButtons>
               </th>
