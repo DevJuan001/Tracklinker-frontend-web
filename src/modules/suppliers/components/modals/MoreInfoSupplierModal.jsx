efbfcc533506444fab31ca39685d238a50f9e8d7
DevJuan001 <juanesyt7@gmail.com>
2026-05-03 14:07:28 -0500

    fix(MoreInfoSupplierModal): refactor supplier details display and replace image with Icon component

--- src/modules/suppliers/components/modals/MoreInfoSupplierModal.jsx
+++ src/modules/suppliers/components/modals/MoreInfoSupplierModal.jsx
@@ -3,6 +3,7 @@ import AddInnerModal from "../../../../globals/components/modals/AddInnerModal";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
 import EditSupplierInfoModal from "./EditSupplierInfoModal";
 import { userStatus } from "../../../users/constants/userStatus";
+import Icon from "../../../../globals/components/ui/Icon";
 
 export default function MoreInfoSupplierModal({ supplier, onClose }) {
   const { innerType, innerTrigger, openInnerModal } = useInnerModal();
@@ -11,17 +12,17 @@ export default function MoreInfoSupplierModal({ supplier, onClose }) {
     <section className="flex flex-col items-center dark:text-white">
       <div className="w-full self-start flex flex-col gap-1.5">
         <div className="flex items-center justify-between">
-          <span className="text-4xl leading-none font-semibold">
+          <span className="text-4xl leading-none font-medium">
             {supplier.name}
           </span>
           <div
             className={`flex gap-1.5 pl-2 py-1.5 rounded-full border
             ${userStatus[supplier.status]?.styles}`}
           >
-            <img
-              src={userStatus[supplier.status]?.icon}
-              alt=""
-              className="w-4 h-4"
+            <Icon
+              name={userStatus[supplier.status]?.icon}
+              size={16}
+              fill={userStatus[supplier.status]?.fill}
             />
             <span className="text-xs font-medium">
               {userStatus[supplier.status]?.text}
@@ -32,37 +33,35 @@ export default function MoreInfoSupplierModal({ supplier, onClose }) {
           <span className="text-sm text-[#7e777ed0] dark:text-[#b4aab4]">
             Teléfono
           </span>
-          <span className="font-medium">{supplier.phone}</span>
+          <span>{supplier.phone}</span>
         </div>
 
         <div className="flex flex-col">
           <span className="text-sm text-[#7e777ed0] dark:text-[#b4aab4]">
             Correo electrónico
           </span>
-          <span className="font-medium">{supplier.email}</span>
+          <span>{supplier.email}</span>
         </div>
 
         <div className="flex flex-col">
           <span className="text-sm text-[#7e777ed0] dark:text-[#b4aab4]">
             Ciudad
           </span>
-          <span className="font-medium">{supplier.city}</span>
+          <span>{supplier.city_name}</span>
         </div>
 
         <div className="flex flex-col">
           <span className="text-sm text-[#7e777ed0] dark:text-[#b4aab4]">
             Dirección
           </span>
-          <span className="font-medium">{supplier.address}</span>
+          <span>{supplier.address}</span>
         </div>
 
         <div className="flex flex-col">
           <span className="text-sm text-[#7e777ed0] dark:text-[#b4aab4]">
             Fecha de creación
           </span>
-          <span className="flex items-center font-medium">
-            {supplier.date}
-          </span>
+          <span>{supplier.date}</span>
         </div>
       </div>
 
