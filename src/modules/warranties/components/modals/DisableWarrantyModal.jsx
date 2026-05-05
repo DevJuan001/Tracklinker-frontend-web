01912184e0d18793f3f9789a0d71668e4a116bd1
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 16:36:28 -0500

    feat(DisableWarrantyModal): update useDisableWarranty hook to accept full warranty object

--- src/modules/warranties/components/modals/DisableWarrantyModal.jsx
+++ src/modules/warranties/components/modals/DisableWarrantyModal.jsx
@@ -8,16 +8,14 @@ import SuccessModal from "../../../../globals/components/modals/SuccessModal";
 import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 
 export default function DisableWarrantyModal({ selectedWarranty, onClose }) {
-  const { handleSubmit, loading } = useDisableWarranty(selectedWarranty.id);
+  const { handleSubmit, loading } = useDisableWarranty(selectedWarranty);
 
   return (
     <div className="flex flex-col items-center">
       <p className="text-center">
-        ¿Estás seguro de que deseas deshabilitar al garantía del producto con el serial {""}
-        <span className="font-medium">
-          {selectedWarranty?.product_serial}
-        </span>
-        ?
+        ¿Estás seguro de que deseas deshabilitar al garantía del producto con el
+        serial {""}
+        <span className="font-medium">{selectedWarranty?.product_serial}</span>?
       </p>
 
       <ConfirmCancelButtons
