161ba37549cc3b0b51209f15fff241bcebf93a0b
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 16:38:29 -0500

    feat(TopSection): add text-nowrap class to section name for improved layout

--- src/globals/components/ui/TopSection.jsx
+++ src/globals/components/ui/TopSection.jsx
@@ -17,7 +17,7 @@ export default function TopSection({
       className={`h-[8%] w-full flex items-center justify-between pb-2 ${sectionVisible ? "block" : "hidden"}
       lg:h-[6%]`}
     >
-      <span className="text-sm md:text-xl font-medium dark:text-white">
+      <span className="text-nowrap text-sm md:text-xl font-medium dark:text-white">
         {sectionName}
       </span>
       <section
