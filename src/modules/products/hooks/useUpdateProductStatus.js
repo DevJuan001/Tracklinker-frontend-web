86cc441e06e2b721095d412a6f81e46847ef7826
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 19:30:12 -0500

    feat(useUpdateProductStatus): refactor form handling and improve error modal integration

--- src/modules/products/hooks/useUpdateProductStatus.js
+++ src/modules/products/hooks/useUpdateProductStatus.js
@@ -1,27 +1,33 @@
 import { useState } from "react";
 import { updateProductStatusService } from "../services/updateProductStatusService";
+import { useQueryClient } from "@tanstack/react-query";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
-export function useUpdateProductStatus(product) {
-  const [form, setForm] = useState({
-    product_id: product.product_id,
-    product_status: 1,
-  });
+export function useUpdateProductStatus(product_data) {
+  const form = product_data;
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const queryClient = useQueryClient();
 
-  async function handleSubmit(e, setInnerModal) {
+  async function handleSubmit(e, onClose, openInnerModal) {
     e.preventDefault();
+
+    const triggerButton = getModalTrigger(e);
+
     setLoading(true);
 
     try {
       const response = await updateProductStatusService(form);
-      if (response.success) {
-        setInnerModal("success");
+      if (response.success === true) {
+        queryClient.invalidateQueries({ queryKey: ["products"] });
+        onClose();
       } else {
-        setInnerModal("error");
+        openInnerModal("error", triggerButton);
+        setError(response.detail)
       }
       setLoading(false);
     } catch (err) {
+      openInnerModal("error", triggerButton);
       setError(err.message);
     } finally {
       setLoading(false);
