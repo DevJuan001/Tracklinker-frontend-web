f9471c848f0016d38c23f33ffbe7cfe935262da7
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 19:28:50 -0500

    feat(ProductStatusConfig): update option styles for better visibility and consistency

--- src/modules/products/constants/productStatusConfig.js
+++ src/modules/products/constants/productStatusConfig.js
@@ -6,17 +6,19 @@ export const productStatusConfig = {
     text: "Deshabilitado",
     modalType: "disable",
     optionText: "Deshabilitar",
-    optionStyles: "hover:bg-red-50 text-red-600 dark:text-red-400",
+    optionStyles:
+      "hover:bg-red-100 text-red-600 dark:hover:bg-[#450a0a8a]",
     visibilityIcon: false,
     icon: usersIcons.inactiveCircle,
     styles:
-      "w-32 bg-gray-100 text-gray-500 dark:bg-gray-800 dark:text-gray-400",
+      "w-32 bg-gray-100 text-gray-500 dark:bg-gray-900 dark:text-gray-400",
   },
   2: {
     text: "Activo",
     modalType: "enable",
     optionText: "Habilitar",
-    optionStyles: "hover:bg-green-50 text-green-600 dark:text-green-400",
+    optionStyles:
+      "hover:bg-green-100 text-green-600 dark:hover:bg-[#082e05b2]",
     visibilityIcon: true,
     icon: usersIcons.activeCircle,
     styles:
@@ -25,7 +27,8 @@ export const productStatusConfig = {
   3: {
     text: "Vendido",
     optionText: "Vender",
-    optionStyles: "hover:bg-blue-50 text-blue-600 dark:text-blue-400",
+    optionStyles:
+      "hover:bg-blue-100 text-blue-600 dark:hover:bg-[#1725548c]",
     visibilityIcon: true,
     icon: productsIcons.paymentCard,
     styles:
@@ -34,11 +37,12 @@ export const productStatusConfig = {
   4: {
     text: "En garantía",
     optionText: "Agregar garantía",
-    optionStyles: "hover:bg-amber-50 text-amber-600 dark:text-amber-400",
+    optionStyles:
+      "hover:bg-amber-100 text-amber-600 dark:text-amber-400 dark:hover:bg-[#4541036b]",
     modalType: "addWarranty",
     visibilityIcon: true,
     icon: productsIcons.clockIcon,
     styles:
-      "w-28 bg-amber-100 text-amber-600 dark:bg-[#2d1f00] dark:text-[#fbbf24]",
+      "w-28 bg-amber-100 text-amber-600 dark:bg-[#2d1f00] dark:text-[#fbbe24b6]",
   },
 };
