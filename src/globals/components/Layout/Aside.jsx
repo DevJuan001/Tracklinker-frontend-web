76fe19f8b1a1860c2fc723688fbbc195431f4109
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 19:27:11 -0500

    feat(Aside): update hover styles for improved accessibility and visual consistency

--- src/globals/components/Layout/Aside.jsx
+++ src/globals/components/Layout/Aside.jsx
@@ -46,7 +46,7 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
                       isActive
                         ? `bg-black font-medium shadow-[0px_0px_32px_-9px_#000000] text-white fill-white
                     dark:bg-white dark:text-black dark:shadow-[0px_0px_32px_-11px_#ffffff] animate-clickEffect`
-                        : `text-[#75777E] font-normal dark:text-[7E8088] hover:bg-gray-200`
+                        : `text-[#75777E] font-normal hover:bg-[#e5e7eb96] dark:text-[#7E8088] dark:hover:bg-[#181818]`
                     }`}
                   >
                     <Icon
@@ -90,7 +90,8 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
                 <button
                   onClick={avatarOnClick}
                   className="w-full h-full flex items-center justify-center py-2.5 px-4 gap-2.5 rounded-full transition duration-300
-                  hover:bg-gray-200 dark:text-gray-50
+                  hover:bg-[#e5e7eb96]
+                  dark:text-[#7E8088] dark:hover:bg-[#181818]
                   xl:justify-start"
                 >
                   <img
@@ -108,7 +109,7 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
                             isActive
                               ? `bg-black font-medium shadow-[0px_0px_32px_-9px_#000000] text-white fill-white
                           dark:bg-white dark:text-black dark:shadow-[0px_0px_32px_-11px_#ffffff] animate-clickEffect`
-                              : `text-[#75777E] font-normal dark:text-[7E8088] hover:bg-gray-200`
+                              : `text-[#75777E] font-normal hover:bg-[#e5e7eb96] dark:text-[#7E8088] dark:hover:bg-[#181818]`
                           }`}
                       >
                         <Icon
@@ -143,7 +144,7 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
             <li
               onClick={avatarOnClick}
               className="w-full h-full flex items-center justify-center py-1.5 gap-2.5 rounded-2xl transition duration-300 cursor-pointer
-            hover:bg-gray-200 
+            hover:bg-[#e5e7eb96] 
             dark:text-gray-50 dark:hover:bg-[#202022]
             xl:justify-start xl:pl-6
             "
@@ -173,7 +174,8 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
                           isActive
                             ? `bg-black font-medium shadow-[0px_0px_32px_-9px_#000000] text-white fill-white
                         dark:bg-white dark:text-black dark:shadow-[0px_0px_32px_-11px_#ffffff] animate-clickEffect`
-                            : `text-[#75777E] font-normal hover:bg-gray-200
+                            : `text-[#75777E] font-normal
+                        hover:bg-[#e5e7eb96]
                         dark:text-[#75777eb7] dark:hover:bg-[#181818]`
                         }`}
                     >
@@ -220,16 +222,16 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
             md:py-3
             xl:py-4"
         >
-          <ul className="flex flex-col gap-[3px]">
+          <ul className="flex flex-col gap-1">
             <span className="text-sm text-[#75777eb7] pb-2 md:pl-1 xl:pl-3">
               Otros
             </span>
             {secondSectionItems.map((item) => (
               <li
                 key={item.name}
-                className="rounded-2xl transition duration-300 text-[#75777E] 
-                hover:bg-gray-200
-                dark:hover:bg-[#3b3b3f98]"
+                className="rounded-2xl transition duration-300 text-[#75777E] font-normal
+                hover:bg-[#e5e7eb96]
+                dark:text-[#7E8088] dark:hover:bg-[#181818]"
               >
                 {item.path ? (
                   <NavLink to={item.path} onClick={item.onClick}>
@@ -257,7 +259,8 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
                 ) : (
                   <button onClick={helpOnClick} className="w-full">
                     <section
-                      className="w-full h-full flex items-center justify-center gap-2.5 py-2.5 pl-7  subpixel-antialiased group
+                      className="w-full h-14 flex items-center justify-center gap-2.5 py-2.5 subpixel-antialiased group
+                      dark:text-[#7E8088]
                       md:pl-0
                       xl:w-full xl:h-auto xl:justify-start xl:pl-7"
                     >
