3f274b7cdf5ef6b523217ba1b8cbef117f86a220
DevJuan001 <juanesyt7@gmail.com>
2026-05-05 19:28:39 -0500

    feat(EnableProductModal): simplify modal logic by removing inner modal states and refactoring submit handler

--- src/modules/products/components/modals/EnableProductModal.jsx
+++ src/modules/products/components/modals/EnableProductModal.jsx
@@ -1,5 +1,4 @@
 // Hooks
-import { useState } from "react";
 import { useUpdateProductStatus } from "../../hooks/useUpdateProductStatus";
 // Components
 import Loader from "../../../../globals/components/ui/Loader";
@@ -8,11 +7,11 @@ import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmC
 import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 import SuccessModal from "../../../../globals/components/modals/SuccessModal";
 
-export default function EnableProductModal({ product, onClose, refetch }) {
-  const [innerModal, setInnerModal] = useState(null);
+export default function EnableProductModal({ product, onClose }) {
   const { handleSubmit, loading } = useUpdateProductStatus({
     product_id: product.product_id,
-    product_status: 2,
+    product_serial: product.product_serial,
+    status: 2,
   });
 
   return (
@@ -27,33 +26,9 @@ export default function EnableProductModal({ product, onClose, refetch }) {
         confirmText={loading ? <Loader /> : "Habilitar"}
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={(e) => handleSubmit(e, setInnerModal)}
+        confirmButtonOnClick={(e) => handleSubmit(e, onClose)}
         cancelButtonOnClick={onClose}
       />
-      {innerModal === "success" && (
-        <SuccessModal
-          isOpen={true}
-          confirmButtonText={"Volver a la página"}
-          confirmTitle={"¡Producto habilitado correctamente!"}
-          confirmText={"El producto ha sido habilitado correctamente."}
-          onClose={() => {
-            setInnerModal(null);
-            refetch();
-            onClose();
-          }}
-        />
-      )}
-      {innerModal === "error" && (
-        <ErrorModal
-          isOpen={true}
-          confirmButtonText={"Volver a intentarlo"}
-          errorTitle={"¡No se pudo habilitar el producto!"}
-          errorText={
-            "Intenta nuevamente habilitar el producto y si el problema persiste comunicate con soporte"
-          }
-          onClose={() => setInnerModal(null)}
-        />
-      )}
     </section>
   );
 }
