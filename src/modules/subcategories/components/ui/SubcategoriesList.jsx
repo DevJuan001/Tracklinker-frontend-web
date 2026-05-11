6f96345b4d67df361079ecb9cbb6bf686b07864d
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 17:36:37 -0500

    feat(SubcategoriesList): enhance empty state with search feedback and create button

--- src/modules/subcategories/components/ui/SubcategoriesList.jsx
+++ src/modules/subcategories/components/ui/SubcategoriesList.jsx
@@ -1,9 +1,12 @@
 import SubcategoriesItem from "./SubcategoriesItem";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import Icon from "../../../../globals/components/ui/Icon";
+import CreateButton from "../../../../globals/components/ui/CreateButton";
 
 export default function SubcategoriesList({
   subcategories,
+  search,
   loading,
   openModal,
 }) {
@@ -12,13 +15,54 @@ export default function SubcategoriesList({
 
   return (
     /* Contenedor de las subcategorías */
-    <section className="max-h-[95%] max-w-full overflow-x-auto overflow-y-auto overflow-hidden">
+    <section className="h-[95%] w-full pb-2 overflow-auto">
+      {noSubcategories && (
+        <div className="w-full h-full flex flex-col items-center justify-center rounded-3xl gap-5">
+          {search !== "" ? (
+            <div
+              className="flex flex-col items-center justify-center gap-2 text-[#7E8088]
+              dark:text-[#E4E2E5]"
+            >
+              <div
+                className="flex items-center justify-center bg-[#F5F3F6] w-24 h-24 rounded-full 
+                dark:bg-[#101012]"
+              >
+                <Icon name={"search_off"} size={60} />
+              </div>
+              <span className="text-xl font-medium text-center">
+                No hay resultados para <strong>"{search}"</strong>. Intenta con
+                otro nombre o crea una nueva.
+              </span>
+            </div>
+          ) : (
+            <div
+              className="flex flex-col items-center justify-center gap-2 text-[#7E8088] 
+              dark:text-[#E4E2E5]"
+            >
+              <div
+                className="flex items-center justify-center bg-[#F5F3F6] w-28 h-28 rounded-full
+                dark:bg-[#101012]"
+              >
+                <Icon name={"folder_copy"} size={60} fill />
+              </div>
+              <div className="flex flex-col items-center">
+                <span className="font-medium text-2xl">
+                  Aún no hay subcategorías
+                </span>
+                <span className="text-lg text-center">
+                  Crea tu primera subcategoría para empezar a organizar tus
+                  productos.
+                </span>
+              </div>
+            </div>
+          )}
+          <CreateButton
+            text={"Crear subcategoria"}
+            onClick={(e) => openModal(null, "add", null, e.currentTarget)}
+          />
+        </div>
+      )}
       <ul className="flex flex-col gap-1">
-        {noSubcategories && (
-          <span className="text-center dark:text-white pt-5">
-            No se encontraron subcategorias
-          </span>
-        )}
         {isFirstLoad ? (
           <SkeletonTheme baseColor="#f3eef5" highlightColor="#c5c1c7">
             <li>
