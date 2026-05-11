4bf164b5643090e485cb73e9dc2ee4d6552d942b
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 15:35:52 -0500

    feat(productsChartData): refactor to use fetchWithAuth for API requests

--- src/modules/dashboard/services/productsChartData.js
+++ src/modules/dashboard/services/productsChartData.js
@@ -1,15 +1,12 @@
 import { apiRoutes } from "../../../config/apiRoutes";
-import { getToken } from "../../../utils/auth";
+import { fetchWithAuth } from "../../../utils/fetchWithAuth";
 
 export async function productsChartData() {
-  const res = await fetch(
+  const res = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.dashboard}/all-and-new`,
     {
       method: "GET",
-      headers: {
-        Authorization: getToken(),
-      },
-    }
+    },
   );
   // Validamos si la respuesta fue OK
   if (!res.ok) {
