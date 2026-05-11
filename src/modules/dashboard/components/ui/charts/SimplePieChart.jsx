61e0c895ea9a40899f4edb46b76d7412e7b34548
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 15:41:15 -0500

    feat(dashboard): refactor SimplePieChart component by removing error handling and simplifying imports

--- src/modules/dashboard/components/ui/charts/SimplePieChart.jsx
+++ src/modules/dashboard/components/ui/charts/SimplePieChart.jsx
@@ -1,18 +1,21 @@
-import { Pie, PieChart, Tooltip, Cell, Legend, ResponsiveContainer } from "recharts";
+import {
+  Pie,
+  PieChart,
+  Tooltip,
+  Cell,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 import { usePieChart } from "../../../hooks/usePieChart";
 import ChartCard from "../ChartCard";
 
 export default function SimplePieChart() {
-  const { simplePieChartData, error } = usePieChart();
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const { simplePieChartData } = usePieChart();
 
   return (
     <ChartCard rowSpan={4} colSpan={3} name={"Estados de garantías"}>
       <div className="w-full h-full min-h-[200px]">
-        <ResponsiveContainer width="100%" height="100%">
+        <ResponsiveContainer>
           <PieChart>
             <Tooltip />
             <Pie
