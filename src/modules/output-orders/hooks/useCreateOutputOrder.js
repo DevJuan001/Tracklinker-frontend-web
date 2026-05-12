afc2856c53512d8d460f16dcba44ce81f13bb281
DevJuan001 <juanesyt7@gmail.com>
2026-05-12 17:38:49 -0500

    feat(useCreateOutputOrder): refactor form handling and submission logic for improved error management

--- src/modules/output-orders/hooks/useCreateOutputOrder.js
+++ src/modules/output-orders/hooks/useCreateOutputOrder.js
@@ -1,51 +1,54 @@
 import { useState } from "react";
 import { createOutputOrderService } from "../services/createOutputOrderService";
+import { useFormValidation } from "../../../globals/hooks/useFormValidation";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
 export function useCreateOutputOrder() {
   const [form, setForm] = useState({
-    out_order_id: "",
     product_serial: "",
-    out_product_garanty: "",
-    product_transformation: "",
+    output_product_garanty: "",
   });
-
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { validate, fieldError, clearError } = useFormValidation();
 
-  // Manejo de inputs
-  const handleChange = (e) => {
+  function handleChange(e) {
     const { name, value } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+    setForm((prev) => ({ ...prev, [name]: value }));
+    clearError(name);
+  }
 
-  // Envío del formulario
-  const handleSubmit = async (e, setInnerModal) => {
+  async function handleSubmit(e, openInnerModal) {
     e.preventDefault();
+
+    const triggerButton = getModalTrigger(e);
+
+    const isValid = validate(form);
+
+    if (!isValid) return;
+
     setLoading(true);
 
     try {
       const response = await createOutputOrderService(form);
-
-      if (!response.success) {
-        console.error("Error API:", response.error);
-        setInnerModal("error");
-        return;
+      if (response.success == true) {
+        openInnerModal("success", triggerButton);
+      } else {
+        openInnerModal("error", triggerButton);
       }
-
-      setInnerModal("success");
     } catch (error) {
-      console.error("Error inesperado:", error);
-      setInnerModal("error");
+      openInnerModal("error", triggerButton);
+      setError(error);
     } finally {
       setLoading(false);
     }
-  };
+  }
 
   return {
     form,
     loading,
+    error,
+    fieldError,
     handleChange,
     handleSubmit,
   };
