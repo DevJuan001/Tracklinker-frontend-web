662ebd49c6a822cc3810d99c3337a06d56f4c44f
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 21:49:13 -0500

    feat(useEnableOutputOrder): implement custom hook for enabling output orders with loading state management

--- src/modules/output-orders/hooks/useEnableOutputOrder.js
+++ src/modules/output-orders/hooks/useEnableOutputOrder.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { enableTransformation } from "../services/enableTransformation.js";
+import { enableOutputOrderService } from "../services/enableOutputOrderService";
 
-export function useEnableTransformation(id) {
+export function useEnableOutputOrder(id) {
   const [loading, setLoading] = useState(false);
 
   async function handleSubmit(e, setInnerModal) {
@@ -10,7 +10,7 @@ export function useEnableTransformation(id) {
     setLoading(true);
 
     try {
-      const response = await enableTransformation(id);
+      const response = await enableOutputOrderService(id);
       if (response.success == true) {
         setInnerModal("success");
       } else {
