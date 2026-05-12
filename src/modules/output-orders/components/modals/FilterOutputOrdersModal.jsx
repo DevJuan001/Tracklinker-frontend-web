779c63d25701afb357a8c3a939bc180408c28da3
DevJuan001 <juanesyt7@gmail.com>
2026-05-11 21:50:47 -0500

    feat(outputOrders): add FilterOutputOrderModal component for filtering output orders

---
+++ src/modules/output-orders/components/modals/FilterOutputOrdersModal.jsx
@@ -0,0 +1,9 @@
+import FilterModal from "../../../../globals/components/modals/FilterModal";
+
+export default function FilterOutputOrderModal({ setFilters, onClose }) {
+  return (
+    <FilterModal onClose={onClose}>
+      <div></div>
+    </FilterModal>
+  );
+}
