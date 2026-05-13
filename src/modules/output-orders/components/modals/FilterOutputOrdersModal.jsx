686f6c0bb2c95b23dae432ad066010b61d6433e4
DevJuan001 <juanesyt7@gmail.com>
2026-05-13 14:31:13 -0500

    feat(FilterOutputOrdersModal): integrate form handling and update modal props for filtering

--- src/modules/output-orders/components/modals/FilterOutputOrdersModal.jsx
+++ src/modules/output-orders/components/modals/FilterOutputOrdersModal.jsx
@@ -1,8 +1,21 @@
 import FilterModal from "../../../../globals/components/modals/FilterModal";
+import { useFilterOutputOrders } from "../../hooks/useFilterOutputOrders";
 
 export default function FilterOutputOrderModal({ setFilters, onClose }) {
+  const { form, handleChange } = useFilterOutputOrders();
+
   return (
-    <FilterModal onClose={onClose}>
+    <FilterModal
+      onClose={onClose}
+      orderByStartDateValue={form.start_date}
+      orderByStartDateOnChange={handleChange}
+      orderByFinishDateValue={form.end_date}
+      orderByFinishDateOnChange={handleChange}
+      applyButtonOnClick={() => {
+        setFilters({ ...form });
+        onClose();
+      }}
+    >
       <div></div>
     </FilterModal>
   );
