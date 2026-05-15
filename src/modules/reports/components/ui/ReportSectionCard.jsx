c670da478ee33397f27c228b39be54fd0ca0967b
DevJuan001 <juanesyt7@gmail.com>
2026-05-15 18:38:48 -0500

    feat(ReportSectionCard): refactor icon handling and update layout structure

--- src/modules/reports/components/ui/ReportSectionCard.jsx
+++ src/modules/reports/components/ui/ReportSectionCard.jsx
@@ -1,9 +1,10 @@
+import Icon from "../../../../globals/components/ui/Icon"
+
 export default function ReportSectionCard({
   sectionOnClick,
   sectionKey,
   sectionName,
-  sectionIcon: Icon,
-  sectionIconAlt,
+  sectionIcon,
 }) {
   return (
     <button
@@ -13,18 +14,19 @@ export default function ReportSectionCard({
         hover:scale-[1.1]
         xl:h-48 xl:w-32"
     >
-      <section
-        className="h-20 w-20 rounded-xl bg-[#eae8eb] content-center justify-items-center
-            dark:bg-[#101012]
-            xl:h-32 xl:w-32
-            md:h-28 md:w-28
-            sm:h-28 sm:w-28"
+      <div
+        className="h-20 w-20 rounded-xl bg-[#eae8eb] flex items-center justify-center
+          dark:bg-[#101012]
+          xl:h-32 xl:w-32
+          sm:h-28 sm:w-28"
       >
         <Icon
-          alt={sectionIconAlt}
-          className="w-10 h-10 fill-[#75777E] dark:invert"
+          name={sectionIcon}
+          size={50}
+          fill
+          color={"#75777E"}
         />
-      </section>
+      </div>
       <p className="text-sm font-medium text-center dark:text-white">
         {sectionName}
       </p>
