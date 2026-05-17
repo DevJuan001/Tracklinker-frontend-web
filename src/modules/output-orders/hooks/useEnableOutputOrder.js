c4d2574400d24127ac4edea09a62e6cfb11a7223
DevJuan001 <juanesyt7@gmail.com>
2026-05-17 18:05:09 -0500

    feat(useEnableOutputOrder): enhance handleSubmit for improved error handling and modal management

--- src/modules/output-orders/hooks/useEnableOutputOrder.js
+++ src/modules/output-orders/hooks/useEnableOutputOrder.js
@@ -1,27 +1,39 @@
 import { useState } from "react";
 import { enableOutputOrderService } from "../services/enableOutputOrderService";
+import { useQueryClient } from "@tanstack/react-query";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
 export function useEnableOutputOrder(id) {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const queryClient = useQueryClient();
 
-  async function handleSubmit(e, setInnerModal) {
+  async function handleSubmit(e, openInnerModal, onClose) {
     e.preventDefault();
+
+    const triggerButton = getModalTrigger(e);
 
     setLoading(true);
 
     try {
       const response = await enableOutputOrderService(id);
+
       if (response.success == true) {
-        setInnerModal("success");
+        queryClient.invalidateQueries({ queryKey: ["outputOrders"] });
+        onClose();
       } else {
-        setInnerModal("error");
+        setError(response.error);
+        openInnerModal("error", triggerButton);
       }
     } catch {
-      setInnerModal("error");
+      setError(
+        "No se pudo realizar esta acción intentalo de nuevo en un momento.",
+      );
+      openInnerModal("error", triggerButton);
     } finally {
       setLoading(false);
     }
   }
 
-  return { handleSubmit, loading };
+  return { loading, error, handleSubmit };
 }
