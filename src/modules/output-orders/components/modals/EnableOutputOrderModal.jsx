9c7c1fb286be11aec93114901271bd6262501655
DevJuan001 <juanesyt7@gmail.com>
2026-05-17 18:14:11 -0500

    feat(EnableOutputOrdersModal): integrate inner modal handling and enhance error display logic

--- src/modules/output-orders/components/modals/EnableOutputOrderModal.jsx
+++ src/modules/output-orders/components/modals/EnableOutputOrderModal.jsx
@@ -1,14 +1,18 @@
-import Loader from "../../../../globals/components/ui/Loader";
+// Hooks
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
 import { useEnableOutputOrder } from "../../hooks/useEnableOutputOrder";
-import ErrorModal from "../../../../globals/components/modals/ErrorModal";
-import SuccessModal from "../../../../globals/components/modals/SuccessModal";
+// Components
+import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+// Modals
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 
 export default function EnableOutputOrdersModal({
   selectedOutputOrder,
   onClose,
 }) {
-  const { handleSubmit, loading } = useEnableOutputOrder(
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+  const { loading, error, handleSubmit } = useEnableOutputOrder(
     selectedOutputOrder.output_order_id,
   );
 
@@ -27,9 +31,21 @@ export default function EnableOutputOrdersModal({
         confirmText={loading ? <Loader /> : "Habilitar"}
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={(e) => handleSubmit(e, onClose)}
+        confirmButtonOnClick={(e) => handleSubmit(e, openInnerModal, onClose)}
         cancelButtonOnClick={onClose}
       />
+
+      {innerType === "error" && (
+        <ErrorModal
+          isOpen={true}
+          location="anchored"
+          growDirection={"top-right"}
+          triggerRef={innerTrigger}
+          errorTitle={"¡No se pudo habilitar la orden de salida!"}
+          errorText={error}
+          onClose={onClose}
+        />
+      )}
     </section>
   );
 }
