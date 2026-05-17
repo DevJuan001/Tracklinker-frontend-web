745961d534bf417af957afd2d482a4af843c20cb
DevJuan001 <juanesyt7@gmail.com>
2026-05-17 18:04:59 -0500

    feat(disableOutputOrderService): improve error handling and response variable naming

--- src/modules/output-orders/services/disableOutputOrderService.js
+++ src/modules/output-orders/services/disableOutputOrderService.js
@@ -2,7 +2,7 @@ import { apiRoutes } from "../../../config/apiRoutes";
 import { fetchWithAuth } from "../../../utils/fetchWithAuth";
 
 export async function disableOutputOrderService(id) {
-  const res = await fetchWithAuth(
+  const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.outputOrders}/disable/${id}`,
     {
       method: "PUT",
@@ -12,9 +12,11 @@ export async function disableOutputOrderService(id) {
     },
   );
 
-  if (!res.ok) {
-    throw new Error("Error en la petición");
+  const json = await response.json();
+
+  if (!response.ok) {
+    return { error: json.detail || "Error en la petición", data: null };
   }
 
-  return await res.json();
+  return json;
 }
