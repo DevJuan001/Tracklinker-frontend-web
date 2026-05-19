62bb9f241235f79c970461c52a44f94b6a2bc5eb
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 18:00:00 -0500

    feat(useDisableSubcategory): enhance error handling and user feedback during subcategory disabling

--- src/modules/subcategories/hooks/useDisableSubcategory.js
+++ src/modules/subcategories/hooks/useDisableSubcategory.js
@@ -1,25 +1,34 @@
 import { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { disableSubcategoryService } from "../services/disableSubcategoryService";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
 export function useDisableSubcategory(subcategory_id) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const queryClient = useQueryClient();
 
-  async function handleSubmit(e, onClose) {
+  async function handleSubmit(e, openInnerModal, onClose) {
     e.preventDefault();
+
+    const triggerButton = getModalTrigger(e);
 
     setLoading(true);
 
     try {
       const response = await disableSubcategoryService(subcategory_id);
       if (response.success === true) {
-        onClose();
         queryClient.invalidateQueries(["subcategories"]);
+        onClose();
+      } else {
+        setError(response.error);
+        openInnerModal("error", triggerButton);
       }
-    } catch (error) {
-      setError(error);
+    } catch {
+      setError(
+        "Por el momento no se puede deshabilitar la subcategoría, por favor intente nuevamente más tarde.",
+      );
+      openInnerModal("error", triggerButton);
     } finally {
       setLoading(false);
     }
