53238768046ba1bdb9db2f4ea6b02dba1becce47
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 16:11:17 -0500

    feat(EnableCategoryModal): integrate error handling and inner modal for category enabling

--- src/modules/categories/components/modals/EnableCategoryModal.jsx
+++ src/modules/categories/components/modals/EnableCategoryModal.jsx
@@ -1,11 +1,15 @@
 // Hooks
 import { useEnableCategory } from "../../hooks/useEnableCategory";
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
 // Componentes
 import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+// Modales
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 
 export default function EnableCategoryModal({ category, onClose }) {
-  const { loading, handleEnable } = useEnableCategory(category.id);
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+  const { loading, error, handleEnable } = useEnableCategory(category.id);
 
   return (
     <section className="flex flex-col justify-center items-center dark:text-white">
@@ -19,9 +23,22 @@ export default function EnableCategoryModal({ category, onClose }) {
         confirmText={loading ? <Loader /> : "Habilitar"}
         confirmDarkBgColor=""
         cancelText={"Cancelar"}
-        confirmButtonOnClick={() => handleEnable(onClose)}
+        confirmButtonOnClick={(e) => handleEnable(e, openInnerModal, onClose)}
         cancelButtonOnClick={onClose}
       />
+
+      {innerType === "error" && (
+        <ErrorModal
+          location="anchored"
+          growDirection={"center-right"}
+          triggerRef={innerTrigger}
+          isOpen={true}
+          errorTitle="¡No se pudo deshabilitar la categoría!"
+          errorText={error}
+          confirmButtonText="Volver a intentarlo"
+          onClose={() => openInnerModal(null)}
+        />
+      )}
     </section>
   );
 }
