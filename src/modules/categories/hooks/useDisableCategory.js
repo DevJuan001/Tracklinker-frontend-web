3230cbf5ad663da8e3695262b2c6448ddf37abc9
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 16:11:39 -0500

    feat(useDisableCategory): refactor handleDisable function for improved error handling and clarity

--- src/modules/categories/hooks/useDisableCategory.js
+++ src/modules/categories/hooks/useDisableCategory.js
@@ -1,30 +1,38 @@
 import { useState } from "react";
 import { disableCategoryService } from "../services/disableCategoryService";
 import { useQueryClient } from "@tanstack/react-query";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
 export function useDisableCategory(id) {
-  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const queryClient = useQueryClient();
 
   // Función que envía el ID al service y maneja la respuesta
-  async function handleDisable(onClose) {
+  async function handleDisable(e, openInnerModal, onClose) {
+    const triggerButton = getModalTrigger(e);
+
     setLoading(true);
 
     try {
       const response = await disableCategoryService(id);
+
       if (response.success === true) {
         queryClient.invalidateQueries({ queryKey: ["categories"] });
         onClose();
+      } else {
+        setError(response.error);
+        openInnerModal("error", triggerButton);
       }
-      setData(response);
-    } catch (error) {
-      setError(error);
+    } catch {
+      openInnerModal("error", triggerButton);
+      setError(
+        "Por el momento no se puede deshabilitar la categoría, por favor intente nuevamente más tarde.",
+      );
     } finally {
       setLoading(false);
     }
   }
 
-  return { data, loading, error, handleDisable };
+  return { loading, error, handleDisable };
 }
