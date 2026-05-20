e00803803affbcc58dd6b8ce194aef8359449d96
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 21:57:49 -0500

    feat(TextArea): update styling by changing border radius and improving className handling

--- src/globals/components/ui/TextArea.jsx
+++ src/globals/components/ui/TextArea.jsx
@@ -6,14 +6,16 @@ export default function TextArea({
   onChange,
   name,
   children,
+  className
 }) {
   return (
     <div
       tabIndex={0}
-      className="relative flex w-full border pr-1 pb-1 rounded-xl shadow-sm
+      className={`relative flex w-full border pr-1 pb-1 rounded-2xl
       focus-within:shadow-[0_0_3px_2px_#e5e7eb]
       dark:border-[#1e1e20cb] dark:focus-within:shadow-[0_0_4px_3px_#28282b]
-      "
+      ${className ? className : "shadow-sm"}
+      `}
     >
       <textarea
         required
