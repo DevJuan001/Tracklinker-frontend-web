d302d9aa16323b48abf2a752510ecb13c4f8daa3
DevJuan001 <juanesyt7@gmail.com>
2026-05-20 16:45:39 -0500

    feat(getWarrantiesService): implement service for fetching warranties with error handling

--- src/modules/warranties/services/getWarrantiesService.js
+++ src/modules/warranties/services/getWarrantiesService.js
@@ -2,7 +2,7 @@ import { apiRoutes } from "../../../config/apiRoutes";
 import { buildQueryParams } from "../../../utils/buildQueryParams";
 import { fetchWithAuth } from "../../../utils/fetchWithAuth";
 
-export async function getWarranties(filters = {}) {
+export async function getWarrantiesService(filters = {}) {
   const params = buildQueryParams(filters);
 
   const res = await fetchWithAuth(
