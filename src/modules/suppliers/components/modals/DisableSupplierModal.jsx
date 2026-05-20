fbef85fbd899be0853f51cf1d598947fb1953a33
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 19:24:36 -0500

    feat(DisableSupplierModal): enhance error handling and improve modal structure

--- src/modules/suppliers/components/modals/DisableSupplierModal.jsx
+++ src/modules/suppliers/components/modals/DisableSupplierModal.jsx
@@ -1,26 +1,42 @@
 // Hooks
 import { useDisableSupplier } from "../../hooks/useDisableSupplier";
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
 // Componentes
 import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+// Modales
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 
 export default function DisableSupplierModal({ supplier, onClose }) {
-  const { loading, handleSubmit } = useDisableSupplier(supplier.id, onClose);
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+  const { loading, error, handleSubmit } = useDisableSupplier(supplier.id);
 
   return (
     <div className="flex flex-col justify-center items-center dark:text-white">
-      <p>
-        ¿Seguro que deseas deshabilitar a {""}
-        <span className="font-medium">{supplier.name}</span>?
-      </p>
+      <span>
+        ¿Seguro que deseas deshabilitar a<strong> {supplier.name}</strong>?
+      </span>
 
       {/* Botones */}
       <ConfirmCancelButtons
         confirmText={loading ? <Loader /> : "Deshabilitar"}
         confirmBgColor="red-600"
-        confirmButtonOnClick={() => handleSubmit()}
+        confirmButtonOnClick={(e) => handleSubmit(e, openInnerModal, onClose)}
         cancelButtonOnClick={onClose}
       />
+
+      {innerType === "error" && (
+        <ErrorModal
+          location="anchored"
+          growDirection={"center-right"}
+          triggerRef={innerTrigger}
+          isOpen={true}
+          errorTitle="¡No se pudo deshabilitar el proveedor!"
+          errorText={error}
+          confirmButtonText="Volver a intentarlo"
+          onClose={() => openInnerModal(null)}
+        />
+      )}
     </div>
   );
 }
