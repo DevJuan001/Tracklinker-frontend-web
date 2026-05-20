2ca8c1a43d4865f0329b07c10e89fa59826f50c9
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 19:25:04 -0500

    feat(useDisableSupplier): improve error handling and modal integration

--- src/modules/suppliers/hooks/useDisableSupplier.js
+++ src/modules/suppliers/hooks/useDisableSupplier.js
@@ -1,23 +1,35 @@
 import { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { disableSupplierService } from "../services/disableSupplierService";
+import { getModalTrigger } from "../../../utils/getModalTrigger";
 
-export function useDisableSupplier(supplier_id, onClose) {
+export function useDisableSupplier(supplier_id) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const queryClient = useQueryClient();
 
-  async function handleSubmit() {
+  async function handleSubmit(e, openInnerModal, onClose) {
+    e.preventDefault();
+
+    const triggerButton = getModalTrigger(e);
+
     setLoading(true);
 
     try {
       const response = await disableSupplierService(supplier_id);
+
       if (response.success === true) {
         queryClient.invalidateQueries({ queryKey: ["suppliers"] });
         onClose();
+      } else {
+        setError(response.error);
+        openInnerModal("error", triggerButton);
       }
-    } catch (error) {
-      setError(error);
+    } catch {
+      setError(
+        "Por el momento no se puede deshabilitar el proveedor, por favor intente nuevamente más tarde.",
+      );
+      openInnerModal("error", triggerButton);
     } finally {
       setLoading(false);
     }
