197d66287fedf892f15415f022d6bbfbf67c3620
DevJuan001 <juanesyt7@gmail.com>
2026-05-19 19:24:42 -0500

    feat(EnableSupplierModal): enhance error handling and improve modal structure

--- src/modules/suppliers/components/modals/EnableSupplierModal.jsx
+++ src/modules/suppliers/components/modals/EnableSupplierModal.jsx
@@ -1,25 +1,40 @@
 // Hooks
 import { useEnableSupplier } from "../../hooks/useEnableSupplier";
+import { useInnerModal } from "../../../../globals/hooks/useInnerModal";
 // Componentes
 import Loader from "../../../../globals/components/ui/Loader";
 import ConfirmCancelButtons from "../../../../globals/components/modals/ConfirmCancelButtons";
+import ErrorModal from "../../../../globals/components/modals/ErrorModal";
 
 export default function EnableSupplierModal({ supplier, onClose }) {
-  const { loading, handleSubmit } = useEnableSupplier(supplier.id, onClose);
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+  const { loading, error, handleSubmit } = useEnableSupplier(supplier.id);
 
   return (
     <div className="flex flex-col justify-center items-center dark:text-white">
-      <p>
-        ¿Seguro que deseas habilitar a {""}
-        <span className="font-medium">{supplier.name}</span>?
-      </p>
+      <span>
+        ¿Seguro que deseas habilitar a<strong> {supplier.name}</strong>?
+      </span>
 
       {/* Botones */}
       <ConfirmCancelButtons
         confirmText={loading ? <Loader /> : "Habilitar"}
-        confirmButtonOnClick={(e) => handleSubmit(e)}
+        confirmButtonOnClick={(e) => handleSubmit(e, openInnerModal, onClose)}
         cancelButtonOnClick={onClose}
       />
+
+      {innerType === "error" && (
+        <ErrorModal
+          location="anchored"
+          growDirection={"center-right"}
+          triggerRef={innerTrigger}
+          isOpen={true}
+          errorTitle="¡No se pudo habilitar el proveedor!"
+          errorText={error}
+          confirmButtonText="Volver a intentarlo"
+          onClose={() => openInnerModal(null)}
+        />
+      )}
     </div>
   );
 }
