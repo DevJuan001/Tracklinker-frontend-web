d3a64b43939686d403321305459af1562925072d
DevJuan001 <juanesyt7@gmail.com>
2026-05-21 15:59:26 -0500

    feat(useFilterUsers): add city filter to form state

--- src/modules/users/hooks/useFilterUsers.js
+++ src/modules/users/hooks/useFilterUsers.js
@@ -6,6 +6,7 @@ export function useFilterUsers(filters) {
     name_order: filters?.name_order || "",
     start_date: filters?.start_date || "",
     end_date: filters?.end_date || "",
+    city: filters?.city || "",
     status: filters?.status || "",
   });
 
