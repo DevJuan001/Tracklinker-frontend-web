fe9c40581301f17abbc4b2497190bca766e34f47
DevJuan001 <juanesyt7@gmail.com>
2026-05-21 15:36:23 -0500

    feat(useFilterUsers): accept filters as parameters for dynamic form initialization

--- src/modules/users/hooks/useFilterUsers.js
+++ src/modules/users/hooks/useFilterUsers.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-export function useFilterUsers() {
+export function useFilterUsers(filters) {
   const [form, setForm] = useState({
-    role_order: "",
-    name_order: "",
-    start_date: "",
-    end_date: "",
-    status: "",
+    role_order: filters?.role_order || "",
+    name_order: filters?.name_order || "",
+    start_date: filters?.start_date || "",
+    end_date: filters?.end_date || "",
+    status: filters?.status || "",
   });
 
   function handleChange(e) {
