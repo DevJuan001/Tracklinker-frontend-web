6537a52565592e0f72b1e3daef1e53c64f8b11b2
DevJuan001 <juanesyt7@gmail.com>
2026-05-22 16:41:31 -0500

    feat: add ProductsTable component for product listing and status management

--- src/modules/login/services/authService.js
+++ src/modules/login/services/authService.js
@@ -1,7 +1,5 @@
 import { apiRoutes } from "../../../config/apiRoutes";
-import { fetchWithAuth } from "../../../utils/fetchWithAuth";
 
-// Función para loguearse
 export async function login(form) {
   const res = await fetch(`${apiRoutes.apiUrl}${apiRoutes.auth}/login`, {
     method: "POST",
@@ -15,24 +13,6 @@ export async function login(form) {
   if (!res.ok) {
     throw new Error("Credenciales Invalidas");
   }
-
-  return await res.json();
-}
-
-// Función para cerrar sesión
-export async function logout(navigate) {
-  const res = await fetchWithAuth(
-    `${apiRoutes.apiUrl}${apiRoutes.auth}/logout`,
-    {
-      method: "POST",
-    },
-  );
-
-  if (!res.ok) {
-    throw new Error("No se pudo eliminar la cookie");
-  }
-
-  navigate("/login");
 
   return await res.json();
 }
