7b61ea43f285b8e10826e15ea670673bb133e94c
DevJuan001 <juanesyt7@gmail.com>
2026-05-22 16:49:35 -0500

    feat: implement mobile and desktop navigation components with associated user profile management modals

---
+++ src/globals/components/Layout/aside/MobileNav.jsx
@@ -0,0 +1,54 @@
+// Hooks
+import { useInnerModal } from "../../../hooks/useInnerModal";
+// Constantes
+import { mobileRelevantItems } from "../../../constants/asideMenuItems";
+// Componentes
+import NavItem from "./NavItem";
+import Icon from "../../ui/Icon";
+import { NavLink } from "react-router-dom";
+// Modales
+import MobileMenuModal from "./MobileMenuModal";
+
+export default function MobileNav({ avatarOnClick, helpOnClick }) {
+  const { innerType, innerTrigger, openInnerModal } = useInnerModal();
+
+  return (
+    <section
+      className="relative w-screen flex justify-between pb-1 pl-1 pr-2
+      sm:px-2
+      md:hidden"
+    >
+      <ul
+        className="w-auto h-full flex px-1 gap-0.5 rounded-full shadow-[1px_1px_10px_5px_#00000014] bg-white
+        dark:bg-black dark:shadow-[1px_1px_1px_4px_#ffffff14]"
+      >
+        {mobileRelevantItems.map((item) => (
+          <li key={item.name} className="py-1.5 rounded-full">
+            <NavItem path={item.path} name={item.name} icon={item.icon} />
+          </li>
+        ))}
+      </ul>
+
+      <button
+        onClick={(e) => openInnerModal("menu", e)}
+        className="self-end w-auto h-16 flex flex-col items-center justify-center py-2.5 px-5 rounded-full bg-black cursor-pointer group
+        dark:bg-white"
+      >
+        <Icon
+          name={"more_horiz"}
+          className="text-white group-hover:text-white dark:text-black dark:group-hover:text-black"
+        />
+      </button>
+
+      {innerType === "menu" && (
+        <MobileMenuModal
+          isOpen={true}
+          triggerRef={innerTrigger}
+          onClose={() => openInnerModal(null)}
+          avatarOnClick={avatarOnClick}
+          helpOnClick={helpOnClick}
+        />
+      )}
+    </section>
+  );
+}
