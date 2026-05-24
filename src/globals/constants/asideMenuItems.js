1d26d1a61ab105521a3cd6952d19bd77106faa02
DevJuan001 <juanesyt7@gmail.com>
2026-05-24 16:36:18 -0500

    feat: add aside menu configuration and role-based access constants

--- src/globals/constants/asideMenuItems.js
+++ src/globals/constants/asideMenuItems.js
@@ -2,7 +2,6 @@ import { asideIcons } from "../../assets/icons/asideIcons";
 
 export const avatarItem = {
   name: "avatar",
-  path: "",
   icon: asideIcons.avatarIcon,
   alt: "Avatar Icon",
 };
@@ -12,52 +11,62 @@ export const firstSectionItems = [
     name: "Inicio",
     path: "/home",
     icon: "home",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Panel",
     secondName: "De Control",
     path: "/dashboard",
     icon: "space_dashboard",
+    roles: ["Admin"],
   },
   {
     name: "Usuarios",
     path: "/users",
     icon: "groups",
+    roles: ["Admin"],
   },
   {
     name: "Productos",
     path: "/products",
     icon: "shopping_cart",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Categorias",
     path: "/categories",
     icon: "folder_open",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Subcategorias",
     path: "/subcategories",
     icon: "folder_copy",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Informes",
     path: "/reports",
     icon: "finance_mode",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Garantías",
     path: "/warranties",
     icon: "service_toolbox",
+    roles: ["Admin", "Técnico"],
   },
   {
     name: "Proveedores",
     path: "/suppliers",
     icon: "group",
+    roles: ["Admin", "Almacén"],
   },
   {
     name: "Ordenes de salida",
     path: "/output-orders",
     icon: "shuffle",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
 ];
 
@@ -78,21 +87,25 @@ export const mobileRelevantItems = [
     name: "Inicio",
     path: "/home",
     icon: "home",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Usuarios",
     path: "/users",
     icon: "groups",
+    roles: ["Admin"],
   },
   {
     name: "Productos",
     path: "/products",
     icon: "shopping_cart",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Informes",
     path: "/reports",
     icon: "finance_mode",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
 ];
 
@@ -101,34 +114,41 @@ export const mobileItems = [
     name: "Categorias",
     path: "/categories",
     icon: "folder_open",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Subcategorias",
     path: "/subcategories",
     icon: "folder_copy",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Garantías",
     path: "/warranties",
     icon: "service_toolbox",
+    roles: ["Admin", "Técnico"],
   },
   {
     name: "Proveedores",
     path: "/suppliers",
     icon: "group",
+    roles: ["Admin", "Almacén"],
   },
   {
     name: "Ordenes",
     path: "/output-orders",
     icon: "shuffle",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Ayuda",
     icon: "feedback",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "Salir",
     path: "/",
     icon: "logout",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
 ];
