4d90956692c7f41f2eb6e13efc9e4f820b098b77
DevJuan001 <juanesyt7@gmail.com>
2026-05-24 16:35:23 -0500

    feat: implement responsive aside navigation with desktop and mobile-specific layouts

--- src/globals/components/Layout/aside/Aside.jsx
+++ src/globals/components/Layout/aside/Aside.jsx
@@ -1,10 +1,10 @@
-import Icon from "../../ui/Icon";
-import DesktopNav from "./DesktopNav";
 import MobileNav from "./MobileNav";
-import Modal from "../../modals/Modal";
+import DesktopNav from "./DesktopNav";
+import { useCurrentUser } from "../../../hooks/useCurrentUser";
 
-// Menú lateral principal de opciones
 export default function Aside({ avatarOnClick, helpOnClick }) {
+  const { user, hasRole } = useCurrentUser();
+
   return (
     <aside
       className="sticky bottom-0 flex order-2 h-[80px] py-1 z-10
@@ -12,8 +12,17 @@ export default function Aside({ avatarOnClick, helpOnClick }) {
       xl:h-full xl:flex-col xl:row-span-2 xl:px-3 xl:pt-5 xl:order-1
       dark:bg-black"
     >
-      <DesktopNav avatarOnClick={avatarOnClick} helpOnClick={helpOnClick} />
-      <MobileNav avatarOnClick={avatarOnClick} helpOnClick={helpOnClick} />
+      <DesktopNav
+        user={user}
+        hasRole={hasRole}
+        avatarOnClick={avatarOnClick}
+        helpOnClick={helpOnClick}
+      />
+      <MobileNav
+        hasRole={hasRole}
+        avatarOnClick={avatarOnClick}
+        helpOnClick={helpOnClick}
+      />
     </aside>
   );
 }
