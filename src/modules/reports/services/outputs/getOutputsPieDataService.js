7f610dbe137491c6f6568a58057b327a230a4866
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:37:46 -0500

    refactor: remove unused signal parameter from output data fetching services

--- src/modules/reports/services/outputs/getOutputsPieDataService.js
+++ src/modules/reports/services/outputs/getOutputsPieDataService.js
@@ -1,18 +1,18 @@
 import { apiRoutes } from "../../../../config/apiRoutes";
 import { fetchWithAuth } from "../../../../utils/fetchWithAuth";
 
-export async function getOutputsPieDataService(period, signal) {
+export async function getOutputsPieDataService(period) {
   const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.reports}/get_outputs_by_brand/${period}`,
     {
       method: "GET",
-      signal,
     },
   );
 
   if (!response.ok) {
     throw new Error("Error en la petición");
   }
+  
   const data = await response.json();
 
   return data.data;
