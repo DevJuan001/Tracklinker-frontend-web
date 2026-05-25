0836c612515c6169c136e63330dffccfcdd5d6f4
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 18:12:38 -0500

    feat: update SubcategoriesTable component to accept data as a prop, enhancing data handling and ensuring compatibility with existing hooks

--- src/modules/reports/components/ui/reports/subcategories/SubcategoriesTable.jsx
+++ src/modules/reports/components/ui/reports/subcategories/SubcategoriesTable.jsx
@@ -2,17 +2,22 @@ import Icon from "../../../../../../globals/components/ui/Icon";
 import { userStatus } from "../../../../../users/constants/userStatus";
 import { useSubcategoriesTableData } from "../../../../hooks/subcategories/useSubCategoriesTableData";
 
-export default function SubcategoriesTable() {
-  const { subcategoriesData } = useSubcategoriesTableData();
+export default function SubcategoriesTable({ data }) {
+  const hookData = useSubcategoriesTableData();
+  const subcategoriesData = data || hookData.subcategoriesData || [];
+
   return (
     <table className="w-full h-full pt-2">
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Nombre</th>
+
           <th className="hidden md:table-cell font-normal text-start pl-4">
             Categoria
           </th>
+
           <th className="font-normal text-start pl-4">Fecha de creación</th>
+
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
