45372a0b61df63ef3eac5b8eebda9286fd2a8484
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:38:04 -0500

    refactor: remove unused signal parameter from subcategory data fetching services

--- src/modules/reports/services/subcategories/getSubcategoriesPieDataService.js
+++ src/modules/reports/services/subcategories/getSubcategoriesPieDataService.js
@@ -1,18 +1,18 @@
 import { apiRoutes } from "../../../../config/apiRoutes";
 import { fetchWithAuth } from "../../../../utils/fetchWithAuth";
 
-export async function getSubcategoriesPieDataService(period, signal) {
+export async function getSubcategoriesPieDataService(period) {
   const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.reports}/get_subcategories_by_category/${period}`,
     {
       method: "GET",
-      signal,
     },
   );
 
   if (!response.ok) {
     throw new Error("Error en la petición");
   }
+  
   const data = await response.json();
 
   return data.data;
