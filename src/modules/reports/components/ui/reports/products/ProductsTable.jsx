815a48a081f2a5b4f86ea48c1c32eb12cd9ed1c1
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 18:12:11 -0500

    feat: update ProductsTable component to accept data as a prop, enhancing data handling and ensuring compatibility with existing hooks

--- src/modules/reports/components/ui/reports/products/ProductsTable.jsx
+++ src/modules/reports/components/ui/reports/products/ProductsTable.jsx
@@ -2,20 +2,26 @@ import Icon from "../../../../../../globals/components/ui/Icon";
 import { productStatusConfig } from "../../../../../products/constants/productStatusConfig";
 import { useProductsTableData } from "../../../../hooks/products/useProductsTableData";
 
-export default function ProductsTable() {
-  const { productsData } = useProductsTableData();
+export default function ProductsTable({ data }) {
+  const hookData = useProductsTableData();
+  const productsData = data || hookData.productsData || [];
+
   return (
     <table className="w-full h-full pt-2">
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Modelo</th>
+
           <th className="hidden md:table-cell font-normal text-start pl-4">
             Serial
           </th>
+
           <th className="hidden md:table-cell font-normal text-start pl-4">
             Marca
           </th>
+
           <th className="font-normal text-start pl-4">Fecha de entrada</th>
+
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
