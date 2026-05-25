3a4a68d8443f5ef70c146afec493b47061ec9aab
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:59:27 -0500

    fix: update ProductsTable to improve responsive design and add unique key prop to rows

--- src/modules/reports/components/ui/reports/products/ProductsTable.jsx
+++ src/modules/reports/components/ui/reports/products/ProductsTable.jsx
@@ -9,21 +9,36 @@ export default function ProductsTable() {
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Modelo</th>
-          <th className="font-normal text-start pl-4">Serial</th>
-          <th className="font-normal text-start pl-4">Marca</th>
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Serial
+          </th>
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Marca
+          </th>
           <th className="font-normal text-start pl-4">Fecha de entrada</th>
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
-      {productsData.map((product) => (
-        <tbody>
-          <tr className="pb-1 text-sm border-b dark:border-[#94909028]">
+      <tbody>
+        {productsData.map((product) => (
+          <tr
+            key={product.serial}
+            className="h-10 pb-1 text-sm border-b dark:border-[#94909028]"
+          >
             <th className="font-normal text-start pl-4">{product.model}</th>
-            <th className="font-normal text-start pl-4">{product.serial}</th>
-            <th className="font-normal text-start pl-4">{product.brand}</th>
+
+            <th className="hidden md:table-cell font-normal text-start pl-4">
+              {product.serial}
+            </th>
+
+            <th className="hidden md:table-cell font-normal text-start pl-4">
+              {product.brand}
+            </th>
+
             <th className="font-normal text-start pl-4">
               {product.input_date}
             </th>
+
             <th className="font-normal text-start pl-4">
               <div
                 className={`flex items-center py-1 px-2 gap-1 rounded-md ${productStatusConfig[product.status]?.styles}`}
@@ -37,8 +52,8 @@ export default function ProductsTable() {
               </div>
             </th>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   );
 }
