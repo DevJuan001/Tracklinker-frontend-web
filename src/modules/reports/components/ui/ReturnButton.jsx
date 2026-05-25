524e51941709fa21403f6149981328075dccd9a9
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:58:43 -0500

    fix: update ReturnButton styles and replace image with Icon component for consistency

--- src/modules/reports/components/ui/ReturnButton.jsx
+++ src/modules/reports/components/ui/ReturnButton.jsx
@@ -1,19 +1,16 @@
-import { actionsIcons } from "../../../../assets/icons/actionsIcons";
+import Icon from "../../../../globals/components/ui/Icon";
 
 export default function ReturnButton({ onClick }) {
   return (
     <button
-      className="flex items-center gap-1 px-3.5 py-1.5 border rounded-md shadow-md transition-all duration-300
+      className="flex items-center gap-1 px-3.5 py-1.5 border rounded-xl shadow-[0px_0px_5px_0.1px_#e5e7eb] transition-all duration-300
       hover:bg-gray-200
-      dark:text-white dark:border-[#9490902d] dark:hover:bg-[#2c2c2e]"
+      dark:text-white dark:border-[#1e1e209f] dark:hover:bg-[#2c2c2e] dark:shadow-[0px_0px_1px_2px_#ffffff20]"
       onClick={onClick}
     >
-      <img
-        src={actionsIcons.arrowBack}
-        alt=""
-        className="w-5 h-5 dark:invert"
-      />
-      <span className="text-sm">Volver</span>
+      <Icon name={"arrow_back"} size={20} />
+
+      <span className="hidden text-sm md:block">Volver</span>
     </button>
   );
 }
