2257f97d3bd009b8d9d0076d47661f131ddf712b
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:58:54 -0500

    fix: update CategoriesTable structure for improved semantic markup and layout

--- src/modules/reports/components/ui/reports/categories/CategoriesTable.jsx
+++ src/modules/reports/components/ui/reports/categories/CategoriesTable.jsx
@@ -10,17 +10,20 @@ export default function CategoriesTable() {
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Nombre</th>
-          <th className="font-normal text-start pl-4">Descripción</th>
+          <th className="hidden md:table-cell font-normal text-start pl-4">Descripción</th>
           <th className="font-normal text-start pl-4">Fecha de creación</th>
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
-      
-      {categoriesData.map((category) => (
-        <tbody>
-          <tr className="pb-1 text-sm border-b dark:border-[#94909028]">
+
+      <tbody>
+        {categoriesData.map((category) => (
+          <tr
+            key={category.name}
+            className="h-10 pb-1 text-sm border-b dark:border-[#94909028]"
+          >
             <th className="font-normal text-start pl-4">{category.name}</th>
-            <th className="font-normal text-start pl-4">
+            <th className="hidden md:table-cell font-normal text-start pl-4">
               {category.description}
             </th>
             <th className="font-normal text-start pl-4">{category.date}</th>
@@ -37,8 +40,8 @@ export default function CategoriesTable() {
               </div>
             </th>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   );
 }
