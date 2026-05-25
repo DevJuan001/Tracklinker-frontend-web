8ae599acf4122dd7e8b812edabe12eec6906eb75
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:58:46 -0500

    fix: update TableCard styles for improved layout and consistency

--- src/modules/reports/components/ui/TableCard.jsx
+++ src/modules/reports/components/ui/TableCard.jsx
@@ -2,12 +2,12 @@ export default function TableCard({ tableTitle, children }) {
   return (
     <section
       key={tableTitle}
-      className="row-span-3 col-span-full bg-white dark:text-white
-        flex flex-col p-5 border border-gray-200 rounded-xl transition duration-500
-        hover:bg-gray-200 hover:scale-[1.01]
-        dark:bg-[#0f0f11] dark:border-transparent dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:hover:bg-[#2c2c2e]"
+      className="row-span-3 col-span-12 bg-white flex flex-col p-5 border border-gray-200 rounded-2xl transition duration-500
+      xl:col-span-full
+      hover:bg-gray-100 hover:scale-[1.005]
+      dark:bg-[#0f0f11] dark:text-white dark:border-[#1e1e209f] dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:hover:bg-[#2c2c2e]"
     >
-      <p className="pb-2">{tableTitle}</p>
+      <span className="pb-2">{tableTitle}</span>
       {children}
     </section>
   );
