a68adfed3c76b970bd028b19cd1a6a3e990d262a
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 16:00:05 -0500

    feat: implement TransformationsReport component with date range selection

--- src/modules/reports/components/ui/reports/transformations/TransformationsReport.jsx
+++ src/modules/reports/components/ui/reports/transformations/TransformationsReport.jsx
@@ -1,5 +1,29 @@
-export default function TransformationsReport() {
-    return(
-        <div></div>
-    );
+// Hooks
+import { useState } from "react";
+// Utils
+import { getDateRange } from "../../../../../../utils/getDateRange";
+// Componentes
+import ReportsContainer from "../../ReportsContainer";
+import ReportsTopSection from "../../ReportsTopSection";
+
+export default function TransformationsReport({ setReport }) {
+  const [period, setPeriod] = useState("1a");
+  const { startDate, endDate } = getDateRange(period);
+
+  return (
+    <section className="w-full h-full flex flex-col gap-2 animate-blurUp">
+      <ReportsTopSection
+        setReport={setReport}
+        periods={["7d", "30d", "6m", "1a"]}
+        setPeriod={setPeriod}
+        currentPeriod={period}
+      />
+
+      <ReportsContainer
+        key={"transformations-reports-container"}
+        reportsName={"transformaciones"}
+        reportsDate={`${startDate} - ${endDate}`}
+      />
+    </section>
+  );
 }
