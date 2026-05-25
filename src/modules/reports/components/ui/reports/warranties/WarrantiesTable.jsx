f06611bd6de26968f46a6ab890a2d3b2d810c8c1
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 16:00:37 -0500

    fix: update WarrantiesTable to conditionally render customer and description columns for improved responsiveness

--- src/modules/reports/components/ui/reports/warranties/WarrantiesTable.jsx
+++ src/modules/reports/components/ui/reports/warranties/WarrantiesTable.jsx
@@ -9,21 +9,35 @@ export default function WarrantiesTable() {
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Serial</th>
-          <th className="font-normal text-start pl-4">Cliente</th>
-          <th className="font-normal text-start pl-4">Descripción</th>
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Cliente
+          </th>
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Descripción
+          </th>
           <th className="font-normal text-start pl-4">Fecha de creación</th>
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
-      {warranties.map((warranty) => (
-        <tbody>
-          <tr className="pb-1 text-sm border-b dark:border-[#94909028]">
+
+      <tbody>
+        {warranties.map((warranty, index) => (
+          <tr
+            key={index}
+            className="pb-1 text-sm border-b dark:border-[#94909028]"
+          >
             <th className="font-normal text-start pl-4">{warranty.serial}</th>
-            <th className="font-normal text-start pl-4">{warranty.customer}</th>
-            <th className="font-normal text-start pl-4">
+
+            <th className="hidden md:table-cell font-normal text-start pl-4">
+              {warranty.customer}
+            </th>
+
+            <th className="hidden md:table-cell font-normal text-start pl-4">
               {warranty.description}
             </th>
+
             <th className="font-normal text-start pl-4">{warranty.date}</th>
+
             <th className="font-normal text-start pl-4">
               <div
                 className={`w-fit flex items-center px-2 py-1 gap-1 rounded-md ${warrantyStatusConfig[warranty.status]?.styles}`}
@@ -37,8 +51,8 @@ export default function WarrantiesTable() {
               </div>
             </th>
           </tr>
-        </tbody>
-      ))}
+        ))}
+      </tbody>
     </table>
   );
 }
