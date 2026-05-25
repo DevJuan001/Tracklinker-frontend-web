a311048f2ccfa21ab371ec3f0cf52fb968f6fa88
DevJuan001 <juanesyt7@gmail.com>
2026-05-24 19:59:44 -0500

    feat: implement ProtectedRoutes component for role-based route authorization

--- src/router/ProtectedRoutes.jsx
+++ src/router/ProtectedRoutes.jsx
@@ -1,37 +1,12 @@
-import { apiRoutes } from "../config/apiRoutes";
-import { useQuery } from "@tanstack/react-query";
+import { useCurrentUser } from "../globals/hooks/useCurrentUser";
 import { Navigate, Outlet } from "react-router-dom";
-import { fetchWithAuth } from "../utils/fetchWithAuth";
 
 export default function ProtectedRoutes({ roles }) {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["roles-verification", roles],
-    queryFn: async () => {
-      const response = await fetchWithAuth(
-        `${apiRoutes.apiUrl}${apiRoutes.auth}/verify-roles`,
-        {
-          method: "POST",
-          credentials: "include",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ roles }),
-        },
-      );
+  const { hasRole, loading, error } = useCurrentUser();
 
-      if (!response.ok) throw new Error("Not authorized");
+  if (loading) return null;
 
-      const result = await response.json();
-
-      return result.success === true;
-    },
-    staleTime: 1000 * 60 * 30,
-    gcTime: 1000 * 60 * 30,
-    retry: false,
-  });
-
-  if (isLoading) return null;
-
-  // Si hay error o no está autorizado
-  if (isError || data === false) return <Navigate to={"/login"} replace />;
+  if (error || !hasRole(roles)) return <Navigate to={"/login"} replace />;
 
   return <Outlet />;
 }
