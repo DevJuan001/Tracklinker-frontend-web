4cf448f569d3b48a74a55d49abdca96fcadde38c
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:36:33 -0500

    feat: add report sections constants for user, product, and category reports

--- src/modules/reports/constants/reportSections.js
+++ src/modules/reports/constants/reportSections.js
@@ -3,35 +3,48 @@ export const sections = [
     name: "users",
     cardName: "Reporte De Usuarios",
     icon: "groups",
+    roles: ["Admin"],
   },
   {
     name: "products",
     cardName: "Reporte De Productos",
     icon: "shopping_cart",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "categories",
     cardName: "Reporte De Categorias",
     icon: "folder_open",
+    roles: ["Admin", "Almacén"],
   },
   {
     name: "subcategories",
     cardName: "Reporte De Subcategorias",
     icon: "folder_copy",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
   {
     name: "warranties",
     cardName: "Reporte De Garantías",
     icon: "service_toolbox",
+    roles: ["Admin", "Técnico"],
   },
   {
     name: "suppliers",
     cardName: "Reporte De Proveedores",
     icon: "group",
+    roles: ["Admin", "Almacén"],
   },
   {
     name: "outputs",
     cardName: "Reporte De Salidas",
     icon: "shuffle",
+    roles: ["Admin", "Almacén", "Técnico"],
+  },
+  {
+    name: "tranformations",
+    cardName: "Reporte de Transformaciones",
+    icon: "swap_horiz",
+    roles: ["Admin", "Almacén", "Técnico"],
   },
 ];
