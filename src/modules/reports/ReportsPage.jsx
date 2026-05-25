5a0ff866333675cfee2f533c4d66cc9dbf7f22e3
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:36:44 -0500

    feat: reorganize report imports and enhance ReportsPage component structure

--- src/modules/reports/ReportsPage.jsx
+++ src/modules/reports/ReportsPage.jsx
@@ -2,18 +2,19 @@
 import { useState } from "react";
 import { useModal } from "../../globals/hooks/useModal";
 // Constantes
-import { sections } from "./data/reportSections";
+import { sections } from "./constants/reportSections";
 // Componentes
 import Layout from "../../globals/components/Layout/Layout";
 import TopSection from "../../globals/components/ui/TopSection";
 import SectionsContainer from "./components/ui/SectionsContainer";
 import UsersReport from "./components/ui/reports/users/UsersReport";
+import OutputsReport from "./components/ui/reports/outputs/OutputsReport";
 import ProductsReport from "./components/ui/reports/products/ProductsReport";
 import SuppliersReport from "./components/ui/reports/suppliers/SuppliersReport";
 import CategoriesReport from "./components/ui/reports/categories/CategoriesReport";
 import WarrantiesReport from "./components/ui/reports/warranties/WarrantiesReport";
 import SubcategoriesReport from "./components/ui/reports/subcategories/SubcategoriesReport";
-import OutputsReport from "./components/ui/reports/outputs/OutputsReport";
+import TransformationsReport from "./components/ui/reports/transformations/TransformationsReport.jsx";
 // Modales
 import Modal from "../../globals/components/modals/Modal";
 import HelpModal from "../../globals/components/modals/HelpModal";
@@ -32,11 +33,12 @@ export default function ReportsPage() {
       }}
     >
       <TopSection
-        sectionVisible={topSectionVisiblity}
-        sectionName={"Informes"}
         filterButton={false}
+        sectionName={"Informes"}
         createButtonVisibility={false}
+        sectionVisible={topSectionVisiblity}
       />
+
       {report === "home" && (
         <SectionsContainer
           sections={sections}
@@ -44,16 +46,27 @@ export default function ReportsPage() {
           setTopSectionVisiblity={setTopSectionVisiblity}
         />
       )}
+
       {/* Contenido principal dinamico */}
       {report === "users" && <UsersReport setReport={setReport} />}
+
       {report === "products" && <ProductsReport setReport={setReport} />}
+
       {report === "categories" && <CategoriesReport setReport={setReport} />}
+
       {report === "subcategories" && (
         <SubcategoriesReport setReport={setReport} />
       )}
+
       {report === "warranties" && <WarrantiesReport setReport={setReport} />}
+
       {report === "suppliers" && <SuppliersReport setReport={setReport} />}
+
       {report === "outputs" && <OutputsReport setReport={setReport} />}
+
+      {report === "transformations" && (
+        <TransformationsReport setReport={setReport} />
+      )}
 
       {/* Modales */}
       {modalType && (
