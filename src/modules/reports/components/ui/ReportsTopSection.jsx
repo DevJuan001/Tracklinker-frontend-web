2b95325747e7d45e1e9676b963f1ccd44e8bdeec
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:58:37 -0500

    fix: update ReportsTopSection styles for improved layout and consistency

--- src/modules/reports/components/ui/ReportsTopSection.jsx
+++ src/modules/reports/components/ui/ReportsTopSection.jsx
@@ -10,20 +10,23 @@ export default function ReportsTopSection({
   return (
     <section className="flex items-center justify-between pl-3 dark:text-white">
       <ReturnButton onClick={() => setReport("home")} />
-      <div className="flex items-center justify-end gap-1.5 pr-3">
+      <div
+        className="flex items-center justify-end gap-1.5
+        md:pr-3"
+      >
         <div
-          className="flex gap-1 py-0.5 px-1 border rounded-xl text-sm font-medium bg-gray-200
-          dark:bg-gray-950 dark:border-neutral-800"
+          className="flex gap-1 py-0.5 px-1 border rounded-2xl text-sm font-medium bg-gray-100
+          dark:bg-black dark:border-none dark:shadow-[0px_0px_1px_2px_#ffffff20]"
         >
           {periods.map((period) => (
             <button
               key={period}
               onClick={() => setPeriod(period)}
-              className={`px-4 py-1.5 rounded-lg 
+              className={`px-4 py-1.5 rounded-xl 
               ${
                 currentPeriod === period
                   ? "bg-white shadow-md dark:text-black"
-                  : "hover:bg-gray-300 transition duration-300 dark:hover:bg-[#2c2c2e]"
+                  : "hover:bg-gray-300 transition duration-200 dark:hover:bg-[#2c2c2e]"
               }`}
             >
               {period}
