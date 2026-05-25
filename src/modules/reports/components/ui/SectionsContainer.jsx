d8994ad7a671b87d733daef62df9a4646ec5aa32
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 14:05:44 -0500

    fix: clean up layout styles in SectionsContainer component for improved readability

--- src/modules/reports/components/ui/SectionsContainer.jsx
+++ src/modules/reports/components/ui/SectionsContainer.jsx
@@ -12,18 +12,11 @@ export default function SectionsContainer({
   return (
     <section
       className="
-        /* Layout Base */
-        h-full grid grid-cols-3 grid-rows-3 place-items-center animate-blurUp
-
-        /* Espaciados */
-        gap-[20px_12px]
-
-        /* Responsive Paddings */
-        px-3 py-4
-        xl:p-[100px_250px_250px_300px] xl:grid-cols-4 xl:grid-rows-2
-        lg:p-[100px_150px_250px_150px]
-        md:p-[50px_50px_200px_50px]
+        h-full grid grid-cols-3 grid-rows-3 place-items-center gap-1 px-3 pb-12 animate-blurUp
         sm:p-[50px_50px_200px_50px]
+        lg:p-[100px_150px_250px_150px]
+        xl:grid-cols-4 xl:grid-rows-2 xl:p-[100px_200px_250px_200px] xl:gap-[20px_12px]
+        
         "
     >
       {sections
