0c01c54e756b5e6b6c2d920736cc071fa37937a5
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 18:09:46 -0500

    feat: update ReportsTopSection to conditionally render periods and pass additional props to ExportButton for enhanced functionality

--- src/modules/reports/components/ui/ReportsTopSection.jsx
+++ src/modules/reports/components/ui/ReportsTopSection.jsx
@@ -4,8 +4,10 @@ import ExportButton from "./ExportButton";
 export default function ReportsTopSection({
   setReport,
   setPeriod,
-  periods,
+  periods = [],
   currentPeriod,
+  openModal,
+  exportData,
 }) {
   return (
     <section className="flex items-center justify-between pl-3 dark:text-white">
@@ -14,26 +16,28 @@ export default function ReportsTopSection({
         className="flex items-center justify-end gap-1.5
         md:pr-3"
       >
-        <div
-          className="flex gap-1 py-0.5 px-1 border rounded-2xl text-sm font-medium bg-gray-100
-          dark:bg-black dark:border-none dark:shadow-[0px_0px_1px_2px_#ffffff20]"
-        >
-          {periods.map((period) => (
-            <button
-              key={period}
-              onClick={() => setPeriod(period)}
-              className={`px-4 py-1.5 rounded-xl 
-              ${
-                currentPeriod === period
-                  ? "bg-white shadow-md dark:text-black"
-                  : "hover:bg-gray-300 transition duration-200 dark:hover:bg-[#2c2c2e]"
-              }`}
-            >
-              {period}
-            </button>
-          ))}
-        </div>
-        <ExportButton />
+        {periods && periods.length > 0 && (
+          <div
+            className="flex gap-1 py-0.5 px-1 border rounded-2xl text-sm font-medium bg-gray-100
+            dark:bg-black dark:border-none dark:shadow-[0px_0px_1px_2px_#ffffff20]"
+          >
+            {periods.map((period) => (
+              <button
+                key={period}
+                onClick={() => setPeriod(period)}
+                className={`px-4 py-1.5 rounded-xl 
+                ${
+                  currentPeriod === period
+                    ? "bg-white shadow-md dark:text-black"
+                    : "hover:bg-gray-300 transition duration-200 dark:hover:bg-[#2c2c2e]"
+                }`}
+              >
+                {period}
+              </button>
+            ))}
+          </div>
+        )}
+        <ExportButton openModal={openModal} exportData={exportData} />
       </div>
     </section>
   );
