afc96641937592478cb5d52a7ee47637d1da0a53
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 18:09:20 -0500

    refactor: simplify button structure in ExportReportModal for improved readability

--- src/modules/reports/components/modals/ExportReportModal.jsx
+++ src/modules/reports/components/modals/ExportReportModal.jsx
@@ -19,27 +19,25 @@ export default function ExportReportModal({ modalData, onClose }) {
 
   return (
     <div className="w-full flex flex-col gap-1.5 p-1">
-      <>
-        <button
-          onClick={handleExportPDF}
-          className="min-h-[52px] w-full flex items-center justify-between px-5 cursor-pointer text-sm rounded-full transition-all duration-200
+      <button
+        onClick={handleExportPDF}
+        className="min-h-[52px] w-full flex items-center justify-between px-5 cursor-pointer text-sm rounded-full transition-all duration-200
             bg-transparent hover:bg-[#efedf0] hover:font-medium active:scale-[0.98]
             dark:text-white dark:hover:bg-[#ffffff15]"
-        >
-          <span>Exportar a PDF</span>
-          <Icon name={"file_save"} size={25} color={"#BB271A"} />
-        </button>
+      >
+        <span>Exportar a PDF</span>
+        <Icon name={"file_save"} size={25} color={"#BB271A"} />
+      </button>
 
-        <button
-          onClick={handleExportExcel}
-          className="min-h-[52px] w-full flex items-center justify-between px-5 cursor-pointer text-sm rounded-full transition-all duration-200
+      <button
+        onClick={handleExportExcel}
+        className="min-h-[52px] w-full flex items-center justify-between px-5 cursor-pointer text-sm rounded-full transition-all duration-200
             bg-transparent hover:bg-[#efedf0] hover:font-medium active:scale-[0.98]
             dark:text-white dark:hover:bg-[#ffffff15]"
-        >
-          <span>Exportar a Excel</span>
-          <Icon name={"table_view"} size={24} color={"#48752C"} />
-        </button>
-      </>
+      >
+        <span>Exportar a Excel</span>
+        <Icon name={"table_view"} size={24} color={"#48752C"} />
+      </button>
     </div>
   );
 }
