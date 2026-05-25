abd1ceae04411f6f5a19aa922e677af714306d75
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 15:59:19 -0500

    fix: update key prop in ProductsPieChart for unique identification of pie chart segments

--- src/modules/reports/components/ui/reports/products/ProductsPieChart.jsx
+++ src/modules/reports/components/ui/reports/products/ProductsPieChart.jsx
@@ -24,7 +24,7 @@ export default function ProductsPieChart({ period }) {
           outerRadius="140"
         >
           {productsData.map((item) => (
-            <Cell key={item.name} fill={item.color} stroke="#1447e6" />
+            <Cell key={item.brand} fill={item.color} stroke="#1447e6" />
           ))}
         </Pie>
       </PieChart>
