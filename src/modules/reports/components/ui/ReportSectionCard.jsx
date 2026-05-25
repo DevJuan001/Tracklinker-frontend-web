433d100b3ac8e50128244c5eff8c8ff48d2265dd
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:36:59 -0500

    fix: correct button hover scale and improve JSX formatting in ReportSectionCard component

--- src/modules/reports/components/ui/ReportSectionCard.jsx
+++ src/modules/reports/components/ui/ReportSectionCard.jsx
@@ -1,4 +1,4 @@
-import Icon from "../../../../globals/components/ui/Icon"
+import Icon from "../../../../globals/components/ui/Icon";
 
 export default function ReportSectionCard({
   sectionOnClick,
@@ -11,8 +11,8 @@ export default function ReportSectionCard({
       onClick={sectionOnClick}
       key={sectionKey}
       className="flex flex-col h-36 w-36 place-items-center gap-2 transition duration-300
-        hover:scale-[1.1]
-        xl:h-48 xl:w-32"
+      hover:scale-[1.05]
+      xl:h-48 xl:w-32"
     >
       <div
         className="h-20 w-20 rounded-xl bg-[#eae8eb] flex items-center justify-center
@@ -20,16 +20,12 @@ export default function ReportSectionCard({
           xl:h-32 xl:w-32
           sm:h-28 sm:w-28"
       >
-        <Icon
-          name={sectionIcon}
-          size={50}
-          fill
-          color={"#75777E"}
-        />
+        <Icon name={sectionIcon} size={50} fill color={"#75777E"} />
       </div>
-      <p className="text-sm font-medium text-center dark:text-white">
+
+      <span className="text-sm font-medium text-center dark:text-white">
         {sectionName}
-      </p>
+      </span>
     </button>
   );
 }
