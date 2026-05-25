985d2cb80ac4384aea6057ce2633051bd9c01832
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 13:38:21 -0500

    refactor: remove unused signal parameter from warranty data fetching services

--- src/modules/reports/services/warranties/getWarrantiesDataService.js
+++ src/modules/reports/services/warranties/getWarrantiesDataService.js
@@ -1,18 +1,18 @@
 import { apiRoutes } from "../../../../config/apiRoutes";
 import { fetchWithAuth } from "../../../../utils/fetchWithAuth";
 
-export async function getWarrantiesDataService(signal) {
+export async function getWarrantiesDataService() {
   const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.reports}/get_warranties_by_status`,
     {
       method: "GET",
-      signal,
     },
   );
 
   if (!response.ok) {
     throw new Error("Error en la petición");
   }
+
   const data = await response.json();
 
   return data.data;
--- src/modules/reports/services/warranties/getWarrantiesTableDataService.js
+++ src/modules/reports/services/warranties/getWarrantiesTableDataService.js
@@ -1,18 +1,18 @@
 import { apiRoutes } from "../../../../config/apiRoutes";
 import { fetchWithAuth } from "../../../../utils/fetchWithAuth";
 
-export async function getWarrantiesTableDataService(signal) {
+export async function getWarrantiesTableDataService() {
   const response = await fetchWithAuth(
     `${apiRoutes.apiUrl}${apiRoutes.reports}/get_recent_warranties`,
     {
       method: "GET",
-      signal,
     },
   );
 
   if (!response.ok) {
     throw new Error("Error en la petición");
   }
+
   const data = await response.json();
 
   return data.data;
