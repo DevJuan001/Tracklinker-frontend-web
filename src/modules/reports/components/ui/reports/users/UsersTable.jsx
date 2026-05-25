72ba8fdf7a90041c2bd6de24ff13b8ca1c28ad62
DevJuan001 <juanesyt7@gmail.com>
2026-05-25 18:14:18 -0500

    feat: update UsersTable component to accept data as a prop, enhancing data handling and ensuring compatibility with existing hooks

--- src/modules/reports/components/ui/reports/users/UsersTable.jsx
+++ src/modules/reports/components/ui/reports/users/UsersTable.jsx
@@ -2,17 +2,26 @@ import Icon from "../../../../../../globals/components/ui/Icon";
 import { userStatus } from "../../../../../users/constants/userStatus";
 import { useUsersTableData } from "../../../../hooks/users/useUsersTableData";
 
-export default function UsersTable() {
-  const { users } = useUsersTableData();
+export default function UsersTable({ data }) {
+  const hookData = useUsersTableData();
+  const users = data || hookData.users || [];
 
   return (
     <table className="w-full h-full pt-2">
       <thead className="h-[30px]">
         <tr className="border-b pb-1 text-sm dark:border-[#94909028]">
           <th className="font-normal text-start pl-4">Nombre</th>
-          <th className="hidden md:table-cell font-normal text-start pl-4">Correo</th>
-          <th className="hidden md:table-cell font-normal text-start pl-4">Número</th>
+
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Correo
+          </th>
+
+          <th className="hidden md:table-cell font-normal text-start pl-4">
+            Número
+          </th>
+
           <th className="font-normal text-start pl-4">Fecha de creación</th>
+
           <th className="font-normal text-start pl-4">Estado</th>
         </tr>
       </thead>
@@ -28,9 +37,13 @@ export default function UsersTable() {
               {user.name} {user.surname}
             </th>
 
-            <th className="hidden md:table-cell font-normal text-start pl-4">{user.email}</th>
+            <th className="hidden md:table-cell font-normal text-start pl-4">
+              {user.email}
+            </th>
 
-            <th className="hidden md:table-cell font-normal text-start pl-4">{user.phone}</th>
+            <th className="hidden md:table-cell font-normal text-start pl-4">
+              {user.phone}
+            </th>
 
             <th className="font-normal text-start pl-4">{user.date}</th>
 
