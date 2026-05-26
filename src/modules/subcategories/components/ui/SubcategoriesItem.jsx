02271805153eacbd7750aa24fb004ca0511b831c
DevJuan001 <juanesyt7@gmail.com>
2026-05-26 16:41:05 -0500

    refactor(SubcategoriesItem): replace <p> with <span> for improved semantic structure and add unique IDs to action buttons for better accessibility

--- src/modules/subcategories/components/ui/SubcategoriesItem.jsx
+++ src/modules/subcategories/components/ui/SubcategoriesItem.jsx
@@ -10,19 +10,21 @@ export default function SubcategoriesItem({
 }) {
   return (
     <li
+      key={subcategory.subcategory_id}
+      onClick={moreInfoOnClick}
       className="flex items-center justify-between p-5 bg-[#F5F3F6] rounded-lg transition duration-300 cursor-pointer
       hover:bg-[#96929231]
       dark:bg-[#101012] dark:hover:bg-[#17171a]"
-      key={subcategory.subcategory_id}
-      onClick={moreInfoOnClick}
     >
       {/* Datos de la Subcategoría */}
       <article>
         <address className="flex items-center gap-3 not-italic font-medium dark:text-white">
-          <p className="text-xl">{subcategory.subcategory_name}</p>
+          <span className="text-xl">{subcategory.subcategory_name}</span>
+
           <div className="flex items-center gap-2">
-            <Icon name={"folder_open"} size={24} className="dark:text-white" />
-            <p>{subcategory.category_name}</p>
+            <Icon name={"folder_open"} size={24} />
+
+            <span>{subcategory.category_name}</span>
           </div>
           <div
             className={`flex items-center px-2 py-1 gap-1 rounded-full text-xs border ${categoryStatusConfig[subcategory.subcategory_status]?.styles}`}
@@ -32,13 +34,17 @@ export default function SubcategoriesItem({
               fill={categoryStatusConfig[subcategory.subcategory_status]?.fill}
               size={14}
             />
+
             <span>
               {categoryStatusConfig[subcategory.subcategory_status]?.text}
             </span>
           </div>
         </address>
       </article>
+
       <ActionButtons
+        editButtonId={`edit-subcategory-${subcategory.subcategory_id}-button`}
+        deleteButtonId={`${categoryStatusConfig[subcategory.subcategory_status]?.modalType}-subcategory-${subcategory.subcategory_id}-button`}
         editButtonOnClick={editButtonOnClick}
         deleteButtonOnClick={(e) => {
           e.stopPropagation();
