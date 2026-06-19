8f36eeeffcba1bf4cf9bc80c6859614fc0eaa578
DevJuan001 <juanesyt7@gmail.com>
2026-06-19 13:56:14 -0500

    feat(modalTitles): add new constants for modal titles in Spanish for warranty management

---
+++ src/modules/warranties/constants/modalTitles.js
@@ -0,0 +1,10 @@
+export const modalTitles = {
+  add: "Crear garantía",
+  info: "",
+  edit: "Editar garantía",
+  disable: "Deshabilitar garantía",
+  enable: "Habilitar garantía",
+  filter: "Filtrar",
+  help: "Ayuda",
+  user: "Configuración",
+};
