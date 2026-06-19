de8de37a79d193f12ff94260c76ed59acdf1f3bd
DevJuan001 <juanesyt7@gmail.com>
2026-06-18 20:42:49 -0500

    feat(SuppliersKpis): add SuppliersKpis component to display key performance indicators for suppliers with loading state

---
+++ src/modules/suppliers/components/ui/SuppliersKpis.jsx
@@ -0,0 +1,39 @@
+import Kpi from "../../../../globals/components/ui/Kpi";
+import Skeleton from "../../../../globals/components/ui/Skeleton";
+import { useSuppliersByStatus } from "../../hooks/useSuppliersByStatus";
+
+export default function SupliersKpis() {
+  const { data, loading } = useSuppliersByStatus();
+
+  return (
+    <section className="h-[10%] mb-3">
+      {loading ? (
+        <div className="flex items-center gap-4">
+          <Skeleton
+            count={4}
+            width="460px"
+            height={"92px"}
+            borderRadius={"20px"}
+            backgroundColor={"#F3EEF5"}
+            darkModeBackgroundColor={"#101012"}
+            shineColor="#C5C1C7"
+            darkModeShineColor="#1e1e1e"
+          />
+        </div>
+      ) : (
+        <div className="h-full flex items-center gap-4">
+          <Kpi
+            title={"Creados recientemente"}
+            value={data?.[0]?.recent_suppliers}
+          />
+
+          <Kpi title={"Activos"} value={data?.[0]?.active_suppliers} />
+
+          <Kpi title={"Deshabilitados"} value={data?.[0].inactive_suppliers} />
+
+          <Kpi title={"Totales"} value={Number(data?.[0]?.total_suppliers)} />
+        </div>
+      )}
+    </section>
+  );
+}
