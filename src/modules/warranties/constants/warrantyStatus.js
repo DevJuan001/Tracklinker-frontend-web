2196b0c1fa20887059b677d96f9d9c6bba448150
DevJuan001 <juanesyt7@gmail.com>
2026-06-19 13:56:36 -0500

    style(warrantyStatus): update styles for warranty status configurations to enhance UI consistency

--- src/modules/warranties/constants/warrantyStatus.js
+++ src/modules/warranties/constants/warrantyStatus.js
@@ -6,7 +6,8 @@ export const warrantyStatusConfig = {
     fill: false,
     optionStyles:
       "hover:bg-red-100 text-red-600 dark:text-[#ff00008e] dark:bg-[#ff00002f]",
-    styles: "bg-gray-50 text-gray-400 dark:text-gray-300 dark:bg-[#ff00002f]",
+    styles: `w-fit flex items-center pl-1.5 pr-3 py-0.5 gap-1.5 rounded-full border bg-gray-50 text-gray-400
+      dark:text-gray-300 dark:bg-[#ff00002f] dark:border-transparent`,
   },
 
   2: {
@@ -16,7 +17,8 @@ export const warrantyStatusConfig = {
     icon: "circle",
     optionStyles:
       "hover:bg-gray-200 text-gray-600 dark:text-[#ff00008e] dark:bg-[#ff00002f]",
-    styles: "bg-red-50 text-red-600 dark:text-[#ff00008e] dark:bg-[#ff00002f]",
+    styles: `w-fit flex items-center pl-1.5 pr-3 py-0.5 gap-1.5 rounded-full border bg-red-50 text-red-600
+    dark:text-[#ff00008e] dark:bg-[#ff00002f] dark:border-transparent`,
   },
 
   3: {
@@ -26,8 +28,8 @@ export const warrantyStatusConfig = {
     fill: true,
     optionStyles:
       "hover:bg-yellow-100 text-yellow-500 dark:text-[#eeff009d] dark:bg-[#fbff001f]",
-    styles:
-      "bg-yellow-50 text-yellow-500 dark:text-[#eeff009d] dark:bg-[#fbff001f]",
+    styles: `w-fit flex items-center pl-1.5 pr-3 py-0.5 gap-1.5 rounded-full border bg-yellow-50 text-yellow-500
+      dark:text-[#eeff009d] dark:bg-[#fbff001f] dark:border-transparent`,
   },
 
   4: {
@@ -37,7 +39,7 @@ export const warrantyStatusConfig = {
     fill: true,
     optionStyles:
       "hover:bg-green-100 text-green-600 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
-    styles:
-      "bg-green-50 text-green-600 dark:text-[#00ff3779] dark:bg-[#00ff151f]",
+    styles: `w-fit flex items-center pl-1.5 pr-3 py-0.5 gap-1.5 rounded-full border bg-green-50 text-green-600
+      dark:text-[#00ff3779] dark:bg-[#00ff151f] dark:border-transparent`,
   },
 };
