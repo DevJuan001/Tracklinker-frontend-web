cab47d6575388727fef65efc8022cd9299bf41c2
DevJuan001 <juanesyt7@gmail.com>
2026-06-19 14:45:01 -0500

    style(Kpi): modify dimensions and spacing for improved layout and visual consistency

--- src/globals/components/ui/Kpi.jsx
+++ src/globals/components/ui/Kpi.jsx
@@ -1,13 +1,12 @@
 export default function Kpi({ title, value }) {
   return (
     <div
-      className="h-full w-full flex flex-col items-start py-3 px-4 gap-1 rounded-2xl bg-[#f5f3f6]
-      md:py-5 md:px-6
+      className="h-[50%] w-[49%] flex flex-col items-start py-3 px-5 gap-1 rounded-3xl bg-[#f5f3f6]
+      md:w-full md:h-full md:py-5 md:px-6
       dark:bg-[#101012]"
     >
       <span
-        className="text-xs text-[#75777E]
-        md:text-sm
+        className="text-sm text-[#75777E]
         dark:text-[#7E8088]"
       >
         {title}
