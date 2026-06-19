6ccaae986877c1a9dcc305b1b89127a9e25d1539
DevJuan001 <juanesyt7@gmail.com>
2026-06-19 14:09:02 -0500

    style(Kpi): adjust padding and font size for improved responsiveness and visual consistency

--- src/globals/components/ui/Kpi.jsx
+++ src/globals/components/ui/Kpi.jsx
@@ -1,11 +1,13 @@
 export default function Kpi({ title, value }) {
   return (
     <div
-      className="h-full w-full flex flex-col items-start py-5 px-6 gap-1 rounded-2xl bg-[#f5f3f6]
+      className="h-full w-full flex flex-col items-start py-3 px-4 gap-1 rounded-2xl bg-[#f5f3f6]
+      md:py-5 md:px-6
       dark:bg-[#101012]"
     >
       <span
-        className="text-sm text-[#75777E]
+        className="text-xs text-[#75777E]
+        md:text-sm
         dark:text-[#7E8088]"
       >
         {title}
