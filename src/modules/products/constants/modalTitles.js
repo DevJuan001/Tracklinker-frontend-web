947564d753e7874db42b1f5867dfef98ad28b9eb
DevJuan001 <juanesyt7@gmail.com>
2026-06-23 15:23:44 -0500

    feat(modalTitles): add constants for modal titles to enhance clarity and maintainability

---
+++ src/modules/products/constants/modalTitles.js
@@ -0,0 +1,10 @@
+export const modalTitles = {
+  user: "Configuración",
+  help: "Ayuda",
+  filter: "Filtrar",
+  add: "Agregar Producto",
+  edit: "Editar Producto",
+  enable: "Habilitar Producto",
+  disable: "Deshabilitar Producto",
+  addWarranty: "Agregar Garantía",
+};
