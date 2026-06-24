8b40f86313e6deaed0eca67a98852ac11923e112
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 18:43:18 -0500

    refactor(dashboard): update ChartsContainer to replace SimpleCharts with MonthlyCharts for improved data visualization

--- src/modules/dashboard/components/ui/ChartsContainer.jsx
+++ src/modules/dashboard/components/ui/ChartsContainer.jsx
@@ -1,17 +1,17 @@
 import UsersKpi from "../kpis/UsersKpi";
 import ProductsKpi from "../kpis/ProductsKpi";
-import BrandsChart from "../charts/BrandsCharts";
+import BrandsChart from "../charts/BrandsChart";
 import CategoriesKpi from "../kpis/CategoriesKpi";
-import SimpleBarChart from "../charts/SimpleBarChart";
-import SimplePieChart from "../charts/SimplePieChart";
-import SimpleAreaChart from "../charts/SimpleAreaChart";
 import OutputOrdersChart from "../kpis/OutputOrdersKpi";
+import MonthlyInputsChart from "../charts/MonthlyInputsChart";
+import WarrantiesPieChart from "../charts/WarrantiesPieChart";
+import MonthlyOutputsChart from "../charts/MonthlyOutputsChart";
 import SubcategoriesWithStockChart from "../charts/SubcategoriesWithStockChart";
 
 export default function ChartsContainer() {
   return (
     <section
-      className="h-[95%] grid grid-cols-1 auto-rows-[minmax(200px,1fr)] gap-5 pb-5 pl-1 transition duration-300 ease-in-out
+      className="h-[95%] grid grid-cols-1 auto-rows-[minmax(200px,1fr)] gap-4 pb-5 pl-1 transition duration-300 ease-in-out
       md:grid-cols-12 md:grid-rows-[repeat(5,minmax(150px,1fr))] overflow-y-auto"
     >
       {/* Primera Fila de Gráficos */}
@@ -25,17 +25,17 @@ export default function ChartsContainer() {
 
       {/* Segunda Fila de Gráficos */}
 
-      <SimpleAreaChart />
+      <MonthlyOutputsChart />
 
       <BrandsChart />
 
-      <SimplePieChart />
+      <WarrantiesPieChart />
 
       {/* Tercera fila de Gráficos */}
 
       <SubcategoriesWithStockChart />
 
-      <SimpleBarChart />
+      <MonthlyInputsChart />
     </section>
   );
 }
