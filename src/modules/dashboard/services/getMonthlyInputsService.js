94ed12e1af2cd369347450e12515693b6aee0979
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 18:42:53 -0500

    feat(dashboard): add getMonthlyInputsService for fetching monthly input data

---
+++ src/modules/dashboard/services/getMonthlyInputsService.js
@@ -0,0 +1,22 @@
+import { apiRoutes } from "../../../config/apiRoutes";
+import { fetchWithAuth } from "../../../utils/fetchWithAuth";
+
+export async function getMonthlyInputsService() {
+  const res = await fetchWithAuth(
+    `${apiRoutes.apiUrl}${apiRoutes.dashboard}/monthly-inputs`,
+    {
+      method: "GET",
+    },
+  );
+
+  // Validamos si la respuesta fue OK
+  if (!res.ok) {
+    throw new Error("Error en la petición");
+  }
+
+  // Convertimos la respuesta a json y la almacenamos en data
+  const data = await res.json();
+
+  // Retornamos la información para el gráfico
+  return data.data;
+}
