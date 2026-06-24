84dcf4e5e9786f76a8738c9e797646695e348c04
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 18:42:59 -0500

    style(dashboard): update SeeReportButton styles for improved visual consistency and responsiveness

--- src/modules/dashboard/components/ui/SeeReportButton.jsx
+++ src/modules/dashboard/components/ui/SeeReportButton.jsx
@@ -4,9 +4,9 @@ export default function SeeReportButton() {
   return (
     <NavLink
       to="/reports"
-      className="self-start p-2 rounded-lg text-sm border transition duration-500 
-    hover:bg-gray-300
-      dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:border-[#ffffff15] dark:hover:bg-[#0f0f11]"
+      className="self-start p-2 rounded-lg text-sm border bg-[#ffffff3d] shadow-[0px_0px_2px_0.5px_#ffffff99] transition duration-200
+      hover:bg-[#E2E4E5]
+      dark:bg-[#0000003d] dark:shadow-[0px_0px_10px_2px_#0f0f11] dark:border-[#ffffff15] dark:hover:bg-[#0f0f11]"
     >
       Ver Informe
     </NavLink>
