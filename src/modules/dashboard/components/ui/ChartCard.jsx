ba4699ee79fd37324563ef4a48b9f5cd2dabe636
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 17:29:46 -0500

    refactor(ChartCard): update markup for better semantic structure and styling consistency

--- src/modules/dashboard/components/ui/ChartCard.jsx
+++ src/modules/dashboard/components/ui/ChartCard.jsx
@@ -21,20 +21,25 @@ export default function ChartCard({
         hover:bg-gray-200 hover:scale-[1.005]
         dark:bg-[#0f0f11] dark:border-[#ffffff10] dark:hover:bg-[#2c2c2e]
         md:col-span-${colSpan} md:row-span-${rowSpan}  
-        `}
+      `}
     >
-      <section className="flex items-start justify-between">
-        <section>
-          <p className="font-medium text-base"> {name} </p>
+      <div className="flex items-start justify-between">
+        <div>
+          <span className="font-medium text-base"> {name} </span>
+
           <div className="flex items-center gap-2">
-            <p className="text-xl font-bold">{metricValue}</p>
-            <p>{percentValue}</p>
+            <span className="text-xl font-bold">{metricValue}</span>
+
+            <span>{percentValue}</span>
           </div>
-        </section>
+        </div>
+
         <SeeReportButton />
-      </section>
+      </div>
+
       <img src={chart} alt="" className={`${imageSize} ${imageDisplay}`} />
-      <div className="h-full">{children}</div>
+
+      {children}
     </section>
   );
 }
