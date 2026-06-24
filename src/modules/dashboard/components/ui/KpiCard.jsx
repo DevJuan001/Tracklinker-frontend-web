40fad5faa2bf10a13030cdfce794d928e8d15f97
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 18:42:48 -0500

    style(dashboard): update KpiCard styles for enhanced visual appeal and consistency

--- src/modules/dashboard/components/ui/KpiCard.jsx
+++ src/modules/dashboard/components/ui/KpiCard.jsx
@@ -3,10 +3,10 @@ import SeeReportButton from "./SeeReportButton";
 export default function KpiCard({ title, background, metricValue, growth }) {
   return (
     <section
-      className={`h-full row-span-1 col-span-1 flex flex-col p-4 shadow-xl border border-gray-200 rounded-xl transition duration-500
+      className={`h-full row-span-1 col-span-1 flex flex-col p-4 shadow-md border border-gray-200 rounded-3xl transition duration-500
         hover:scale-[1.005]
         md:col-span-3
-        dark:bg-[#0f0f11] dark:border-[#ffffff10] dark:text-white ${background}`}
+        dark:bg-[#101012b9] dark:border-[#ffffff10] dark:text-white ${background}`}
     >
       <div className="flex items-center justify-between">
         {/* Nombre del Grafico */}
