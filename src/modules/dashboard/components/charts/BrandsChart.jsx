b29e568fbb2e4c0d67466f260252de2a26004f88
DevJuan001 <juanesyt7@gmail.com>
2026-06-24 18:18:53 -0500

    feat(dashboard): add BrandsChart component to visualize brand data with a bar chart

--- src/modules/dashboard/components/charts/BrandsChart.jsx
+++ src/modules/dashboard/components/charts/BrandsChart.jsx
@@ -14,18 +14,13 @@ export default function BrandsChart() {
 
   return (
     <ChartCard rowSpan={2} colSpan={3} name={"Marcas con más unidades"}>
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer> 
         <BarChart data={brandChartInfo}>
           <YAxis width={10} fontSize={11} />
 
           <XAxis dataKey={"brand"} fontSize={"8px"} />
 
-          <Bar
-            dataKey={"products"}
-            fill="#152DD1"
-            background={"#000"}
-            radius={[8, 8, 0, 0]}
-          />
+          <Bar dataKey={"products"} fill="#152DD1" radius={[8, 8, 0, 0]} />
 
           <Tooltip />
         </BarChart>
