4c5f54abc71d29e873cc8c58836607220a25a99d
DevJuan001 <juanesyt7@gmail.com>
2026-06-29 16:10:14 -0500

    feat(modalTitles): add modal titles constants for user-related modals

---
+++ src/modules/users/constants/modalTitles.js
@@ -0,0 +1,9 @@
+export const modalTitles = {
+  user: "Configuración",
+  help: "Ayuda",
+  filter: "Filtrar",
+  add: "Crear usuario",
+  edit: "Editar usuario",
+  enable: "Habilitar usuario",
+  disable: "Deshabilitar usuario",
+};
