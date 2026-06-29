a356701bbc3cb4edf4830e9993beab8ba31bc872
DevJuan001 <juanesyt7@gmail.com>
2026-06-29 13:22:34 -0500

    feat(categories): add modalTitles constant for category management modals

---
+++ src/modules/categories/constants/modalTitles.js
@@ -0,0 +1,9 @@
+export const modalTitles = {
+    user: "Configuración",
+    help: "Ayuda",
+    filter: "Filtrar",
+    add: "Crear categoría",
+    edit: "Editar categoría",
+    enable: "Habilitar categoría",
+    disable: "Deshabilitar categoría",
+  };
