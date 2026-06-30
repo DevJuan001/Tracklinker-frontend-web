97e77e1884e921663cd507d5c40b77c0d6b4325f
DevJuan001 <juanesyt7@gmail.com>
2026-06-30 16:39:47 -0500

    refactor(categories): replace modalTitles with modals constant for improved modal management

---
+++ src/modules/categories/constants/modals.js
@@ -0,0 +1,10 @@
+export const modals = {
+  user: { title: "Configuración" },
+  help: { title: "Ayuda" },
+  filter: { title: "Filtrar" },
+  info: { location: "center" },
+  add: { title: "Crear categoría", location: "center" },
+  edit: { title: "Editar categoría" },
+  enable: { title: "Habilitar categoría" },
+  disable: { title: "Deshabilitar categoría" },
+};
