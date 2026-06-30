1e273e16d93ebbe5b75c0ba13cb0d97f09eaa53c
DevJuan001 <juanesyt7@gmail.com>
2026-06-30 18:48:22 -0500

    refactor(products): centralize modal titles and locations in a new modals constants file

---
+++ src/modules/products/constants/modals.js
@@ -0,0 +1,10 @@
+export const modals = {
+  user: { title: "Configuración", location: "center" },
+  help: { title: "Ayuda", location: "center" },
+  filter: { title: "Filtrar" },
+  add: { title: "Agregar Producto", location: "center" },
+  edit: { title: "Editar Producto", location: "center" },
+  enable: { title: "Habilitar Producto", location: "center" },
+  disable: { title: "Deshabilitar Producto", location: "center" },
+  addWarranty: { title: "Agregar Garantía", location: "center" },
+};
