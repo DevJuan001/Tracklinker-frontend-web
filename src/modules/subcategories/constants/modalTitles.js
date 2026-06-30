9a7eab169187e09c688b6f16afe5f6e3d1cc9756
DevJuan001 <juanesyt7@gmail.com>
2026-06-30 18:53:22 -0500

    refactor(subcategories): centralize modal titles and locations in a new modals constants file

--- src/modules/subcategories/constants/modalTitles.js
+++
@@ -1,9 +0,0 @@
-export const modalTitles = {
-  user: "Configuración",
-  help: "Ayuda",
-  filter: "Filtrar",
-  add: "Agregar subcategoría",
-  edit: "Editar subcategoría",
-  enable: "Habilitar subcategoría",
-  disable: "Deshabilitar subcategoría",
-};
