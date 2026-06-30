450f53c90f49b7ce1be52445328aceca5513b7b5
DevJuan001 <juanesyt7@gmail.com>
2026-06-30 15:10:57 -0500

    feat(modalTitles): add modal titles constants for subcategory management

---
+++ src/modules/subcategories/constants/modalTitles.js
@@ -0,0 +1,9 @@
+export const modalTitles = {
+  user: "Configuración",
+  help: "Ayuda",
+  filter: "Filtrar",
+  add: "Agregar subcategoría",
+  edit: "Editar subcategoría",
+  enable: "Habilitar subcategoría",
+  disable: "Deshabilitar subcategoría",
+};
